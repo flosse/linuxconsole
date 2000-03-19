0f3288ccf34ecd5fd3c31410db73939ae8f51d8d
Vojtech Pavlik <vojtech@suse.cz>
2000-03-19 16:08:40 +0000

    These are the utilities useful for testing the input drivers. Inputattach serves as a line discipline attach progtam (a la slattach) for serial mice and other serial devices.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@27 81ba4a36-902d-0410-9329-aea0610344cf

---
+++ ruby/utils/evtest.c
@@ -0,0 +1,114 @@
+/*
+ *  evtest.c  Version 0.1
+ *
+ *  Copyright (c) 1999 Vojtech Pavlik
+ *
+ *  Event device test program
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Should you need to contact me, the author, you can do so either by
+ * e-mail - mail your message to <vojtech@ucw.cz>, or by paper mail:
+ * Vojtech Pavlik, Ucitelska 1576, Prague 8, 182 00 Czech Republic
+ */
+
+#include <linux/input.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+char *events[EV_MAX + 1] = { "Reset", "Key", "Relative", "Absolute", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+NULL, NULL, NULL, "LED", "Sound", "Repeat" };
+char *keys[KEY_MAX + 1] = { "Reserved", "Esc", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Minus", "Equal", "Backspace",
+"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "LeftBrace", "RightBrace", "Enter", "LeftControl", "A", "S", "D", "F", "G",
+"H", "J", "K", "L", "Semicolon", "Apostrophe", "Grave", "LeftShift", "BackSlash", "Z", "X", "C", "V", "B", "N", "M", "Comma", "Dot",
+"Slash", "RightShift", "KPAsterisk", "LeftAlt", "Space", "CapsLock", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",
+"NumLock", "ScrollLock", "KP7", "KP8", "KP9", "KPMinus", "KP4", "KP5", "KP6", "KPPlus", "KP1", "KP2", "KP3", "KP0", "KPDot", "103rd",
+"F13", "102nd", "F11", "F12", "F14", "F15", "F16", "F17", "F18", "F19", "F20", "KPEnter", "RightCtrl", "KPSlash", "SysRq",
+"RightAlt", "LineFeed", "Home", "Up", "PageUp", "Left", "Right", "End", "Down", "PageDown", "Insert", "Delete", "Macro", "Mute",
+"VolumeDown", "VolumeUp", "Power", "KPEqual", "KPPlusMinus", "Pause", "F21", "F22", "F23", "F24", "JPN", "LeftMeta", "RightMeta",
+"Compose", "Stop", "Again", "Props", "Undo", "Front", "Copy", "Open", "Paste", "Find", "Cut", "Help", "Menu", "Calc", "Setup",
+"Sleep", "WakeUp", "File", "SendFile", "DeleteFile", "X-fer", "Prog1", "Prog2", "WWW", "MSDOS", "Coffee", "Direction",
+"CycleWindows", "Mail", "Bookmarks", "Computer", "Back", "Forward", "CloseCD", "EjectCD", "EjectCloseCD", "NextSong", "PlayPause",
+"PreviousSong", "StopCD", "Record", "Rewind", "Phone", NULL, "Config", "HomePage", "Refresh", "Exit", "Move", "Edit", "ScrollUp",
+"ScrollDown",
+NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+"Btn0", "Btn1", "Btn2", "Btn3", "Btn4", "Btn5", "Btn6", "Btn7", "Btn8", "Btn9", NULL, NULL,  NULL, NULL, NULL, NULL,
+"LeftBtn", "RightBtn", "MiddleBtn", "SideBtn", "ExtraBtn", "ForwardBtn", "BackBtn",
+NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+"Trigger", "ThumbBtn", "ThumbBtn2", "TopBtn", "TopBtn2", "BaseBtn", "BaseBtn2", "BaseBtn3", "BaseBtn4", "BaseBtn5", "BaseBtn6",
+NULL, NULL, NULL, NULL, NULL, NULL,
+"BtnA", "BtnB", "BtnC", "BtnX", "BtnY", "BtnZ", "BtnTL", "BtnTR", "BtnTL2", "BtnTR2", "BtnSelect", "BtnStart", "BtnMode",
+NULL, NULL,
+"ToolPen", "ToolRubber", "ToolBrush", "ToolPencil", "ToolAirbrush", "ToolFinger", "ToolMouse", "ToolLens", NULL, NULL,
+"Touch", "Stylus", "Stylus2" };
+
+char *relatives[REL_MAX + 1] = { "X", "Y", "Z", NULL, NULL, NULL, "HWheel", "Dial", "Wheel" };
+char *absolutes[ABS_MAX + 1] = { "X", "Y", "Z", "Rx", "Ry", "Rz", "Throttle", "Rudder", NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+NULL, "Hat0X", "Hat0Y", "Hat1X", "Hat1Y", "Hat2X", "Hat2Y", "Hat3X", "Hat 3Y", "Pressure", "Distance", "XTilt", "YTilt"};
+char *leds[LED_MAX + 1] = { "NumLock", "CapsLock", "ScrollLock", "Compose", "Kana" };
+char *repeats[REP_MAX + 1] = { "Delay", "Period" };
+char *sounds[SND_MAX + 1] = { "Bell", "Click" };
+
+char **names[EV_MAX + 1] = { events, keys, relatives, absolutes, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+NULL, NULL, leds, sounds, repeats };
+
+int main (int argc, char **argv)
+{
+	int fd, rd, i;
+	struct input_event ev[64];
+
+	if (argc < 2) {
+		printf ("Usage: evtest /dev/inputX\n");
+		printf ("Where X = input device number\n");
+		exit (1);
+	}
+
+#if 0
+	for (i = 0; i < KEY_MAX; i++)
+		printf("%d - %s\n", i, keys[i]);
+#endif
+
+	if ((fd = open(argv[argc - 1], O_RDONLY)) < 0) {
+		perror("evtest");
+		exit(1);
+	}
+
+	printf("Testing ... (interrupt to exit)\n");
+
+	while (1) {
+		rd = read(fd, ev, sizeof(struct input_event) * 64);
+
+		if (rd < sizeof(struct input_event)) {
+			perror("\nevtest: error reading");
+			exit (1);
+		}
+
+		for (i = 0; i < rd / sizeof(struct input_event); i++)
+			printf("Event: time %ld.%06ld, type %d (%s), code %d (%s), value %d\n",
+				ev[i].time.tv_sec, ev[i].time.tv_usec, ev[i].type,
+				events[ev[i].type] ? events[ev[i].type] : "?",
+				ev[i].code,
+				names[ev[i].type] ? (names[ev[i].type][ev[i].code] ? names[ev[i].type][ev[i].code] : "?") : "?",
+				ev[i].value);
+
+	}
+}
---
+++ ruby/utils/inputattach.c
@@ -0,0 +1,214 @@
+/*
+ *  inputattach.c  Version 0.1
+ *
+ *  Copyright (c) 1999 Vojtech Pavlik
+ *
+ *  Input line discipline attach program
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Should you need to contact me, the author, you can do so either by
+ * e-mail - mail your message to <vojtech@ucw.cz>, or by paper mail:
+ * Vojtech Pavlik, Ucitelska 1576, Prague 8, 182 00 Czech Republic
+ */
+
+#include <linux/serio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <string.h>
+
+int readchar(int fd, unsigned char *c, int timeout)
+{
+	struct timeval tv;
+	fd_set set;
+
+	tv.tv_sec = 0;
+	tv.tv_usec = timeout * 1000;
+
+	FD_ZERO(&set);
+	FD_SET(fd, &set);
+
+	if (!select(fd+1, &set, NULL, NULL, &tv)) return -1;
+
+	read(fd, c, 1);
+
+	return 0;
+}
+
+void setline(int fd, int flags, int speed)
+{
+	struct termios t;
+
+	tcgetattr(fd, &t);
+
+	t.c_cflag = flags | CREAD | HUPCL | CLOCAL;
+	t.c_iflag = IGNBRK | IGNPAR;
+	t.c_oflag = 0;
+	t.c_lflag = 0;
+	t.c_cc[VMIN ] = 1;
+	t.c_cc[VTIME] = 0;
+
+	cfsetispeed(&t, speed);
+	cfsetospeed(&t, speed);
+
+	tcsetattr(fd, TCSANOW, &t);
+}
+
+int logitech_command(int fd, char *c)
+{
+	int i;
+	unsigned char d;
+	for (i = 0; c[i]; i++) {
+		write(fd, c + i, 1);
+		if (readchar(fd, &d, 1000))
+			return -1;
+		if (c[i] != d)
+			return -1;
+	}
+	return 0;
+}
+
+int magellan_init(int fd)
+{
+	write(fd, "m3\rpBB\rz\r", 11);
+	return 0;
+}
+
+int warrior_init(int fd)
+{
+	if (logitech_command(fd, "*S")) return -1;
+	setline(fd, CS8, B4800);
+	return 0;
+}
+
+int mzp_init(int fd)
+{
+	if (logitech_command(fd, "*X*q")) return -1;
+	setline(fd, CS8, B9600);
+	return 0;
+}
+
+struct input_types {
+	char name[16];
+	char name2[16];
+	int speed;
+	int flags;
+	unsigned long type;
+	unsigned long extra;
+	int (*init)(int fd);
+};
+
+struct input_types input_types[] = {
+
+{ "--sunkbd",		"-skb",		B1200, CS8,			SERIO_SUNKBD,	0x00,	NULL },
+{ "--spaceorb",		"-orb",		B9600, CS8,			SERIO_SPACEORB,	0x00,	NULL },
+{ "--magellan",		"-mag",		B9600, CS8 | CSTOPB | CRTSCTS,	SERIO_MAGELLAN,	0x00,	magellan_init },
+{ "--warrior",		"-war",		B1200, CS7 | CSTOPB,		SERIO_WARRIOR,	0x00,	warrior_init },
+{ "--mousesystems",	"-msc",		B1200, CS8,			SERIO_MSC,	0x01,	NULL },
+{ "--sunmouse",		"-sun",		B1200, CS8,			SERIO_SUN,	0x01,	NULL },
+{ "--microsoft",	"-bare",	B1200, CS7,			SERIO_MS,	0x00,	NULL },
+{ "--mshack",		"-ms",		B1200, CS7,			SERIO_MS,	0x01,	NULL },
+{ "--mouseman",		"-mman",	B1200, CS7,			SERIO_MP,	0x01,	NULL },
+{ "--intellimouse",	"-ms3",		B1200, CS7,			SERIO_MZ,	0x11,	NULL },
+{ "--mmwheel",		"-mmw",		B1200, CS7 | CSTOPB,		SERIO_MZP,	0x13,	mzp_init },
+{ "", "", 0, 0 }
+
+};
+
+int main(int argc, char **argv)
+{
+	unsigned long devt;
+	int ldisc;
+        int type;
+        int fd;
+	char c;
+
+        if (argc < 2 || argc > 3 || !strcmp("--help", argv[1])) {
+                puts("");
+                puts("Usage: inputttach <mode> <device>");
+                puts("");
+                puts("Modes:");
+                puts("  --sunkbd        -skb   Sun Type 4 and Type 5 keyboards");
+                puts("  --spaceorb      -orb   SpaceOrb 360 / SpaceBall Avenger");
+                puts("  --magellan      -mag   Magellan / SpaceMouse");
+                puts("  --warrior       -war   WingMan Warrior");
+		puts("  --mousesystems  -msc   3-button Mouse Systems mice");
+		puts("  --sunmouse      -sun   3-button Sun mice");
+		puts("  --microsoft     -bare  2-button Microsoft mice");
+		puts("  --mshack        -ms    3-button mice in Microsoft mode");
+		puts("  --mouseman      -mman  3-button Logitech and Genius mice");
+		puts("  --intellimouse  -ms3   Microsoft IntelliMouse");
+		puts("  --mmwheel       -mmw   Logitech mice with 4-5 buttons or wheel");
+                puts("");
+                return 1;
+        }
+
+        for (type = 0; input_types[type].speed; type++) {
+                if (!strncasecmp(argv[1], input_types[type].name, 16) ||
+			!strncasecmp(argv[1], input_types[type].name2, 16))
+                        break;
+        }
+
+	if (!input_types[type].speed) {
+		fprintf(stderr, "inputattach: invalid mode\n");
+		return 1;
+	}
+
+	if ((fd = open(argv[2], O_RDWR | O_NOCTTY | O_NONBLOCK)) < 0) {
+		perror("inputattach");
+		return 1;
+	}
+
+	setline(fd, input_types[type].flags, input_types[type].speed);
+
+	while (!readchar(fd, &c, 100));
+
+	if (input_types[type].init && input_types[type].init(fd)) {
+		fprintf(stderr, "inputattach: device initialization failed\n");
+		return 1;
+	}
+
+	ldisc = N_MOUSE;
+	if(ioctl(fd, TIOCSETD, &ldisc)) {
+		fprintf(stderr, "inputattach: can't set line discipline\n"); 
+		return 1;
+	}
+
+	devt = SERIO_RS232 | input_types[type].type | (input_types[type].extra << 16);
+
+	if(ioctl(fd, SPIOCSTYPE, &devt)) {
+		fprintf(stderr, "inputattach: can't set device type\n");
+		return 1;
+	}
+
+	read(fd, NULL, 0);
+
+	ldisc = 0;
+	ioctl(fd, TIOCSETD, &ldisc);
+	close(fd);
+
+	return 0;
+}
