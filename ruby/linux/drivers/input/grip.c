2f8849e280bbfcae9fa66aa7caeccc2880f4d7f9
Vojtech Pavlik <vojtech@suse.cz>
2000-04-14 08:32:31 +0000

    Working on joystick drivers.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@146 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/drivers/input/grip.c
+++ ruby/linux/drivers/input/grip.c
@@ -1,121 +1,121 @@
 /*
- *  joy-gravis.c  Version 1.2
+ *  grip.c  Version 1.3
  *
- *  Copyright (c) 1998-1999 Vojtech Pavlik
+ *  Copyright (c) 1998-2000 Vojtech Pavlik
  *
  *  Sponsored by SuSE
  */
 
 /*
- * This is a module for the Linux joystick driver, supporting
- * Gravis GrIP digital joystick family.
+ * Gravis/Kensington GrIP protocol joystick and gamepad driver for Linux
  */
 
 /*
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or 
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * 
+ *
  * Should you need to contact me, the author, you can do so either by
  * e-mail - mail your message to <vojtech@suse.cz>, or by paper mail:
  * Vojtech Pavlik, Ucitelska 1576, Prague 8, 182 00 Czech Republic
  */
 
-#include <asm/io.h>
-#include <asm/system.h>
-#include <linux/errno.h>
-#include <linux/ioport.h>
-#include <linux/joystick.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/string.h>
 #include <linux/init.h>
+#include <linux/gameport.h>
+#include <linux/input.h>
 
-#define JS_GR_MODE_GPP		1
-#define JS_GR_LENGTH_GPP	24
-#define JS_GR_STROBE_GPP	400
+#define GRIP_MODE_GPP		1
+#define GRIP_MODE_XT		2
+#define GRIP_MODE_BD		3
 
-#define JS_GR_MODE_XT		2
-#define JS_GR_MODE_BD		3
-#define JS_GR_LENGTH_XT		4
-#define JS_GR_STROBE_XT		200
-#define JS_GR_MAX_CHUNKS_XT	10	
-#define JS_GR_MAX_BITS_XT	30	
+#define GRIP_LENGTH_GPP	24
+#define GRIP_STROBE_GPP	400
+#define GRIP_LENGTH_XT		4
+#define GRIP_STROBE_XT		200
+#define GRIP_MAX_CHUNKS_XT	10	
+#define GRIP_MAX_BITS_XT	30	
 
-static int js_gr_port_list[] __initdata = {0x201, 0};
-static struct js_port* js_gr_port __initdata = NULL;
-
-struct js_gr_info {
-	int io;
+struct grip {
+	struct gameport *gameport;
+	struct timer_list timer;
+	struct input_dev dev[2];
 	unsigned char mode[2];
+	int used;
+	int reads;
+	int bads;
 };
 
 /*
- * js_gr_gpp_read_packet() reads a Gravis GamePad Pro packet.
+ * grip_gpp_read_packet() reads a Gravis GamePad Pro packet.
  */
 
-static int js_gr_gpp_read_packet(int io, int shift, unsigned int *data)
+static int grip_gpp_read_packet(struct gameport *gameport, int shift, unsigned int *data)
 {
 	unsigned long flags;
 	unsigned char u, v;
-	unsigned int t, p;
+	unsigned int t;
 	int i;
 
-	i = 0;
+	int strobe = gameport_time(GRIP_STROBE_GPP);
+
 	data[0] = 0;
-	p = t = JS_GR_STROBE_GPP;
-	p += JS_GR_STROBE_GPP;
+	t = strobe;
+	i = 0;
 
 	__save_flags(flags);
 	__cli();
 
-	v = inb(io) >> shift;
+	v = gameport_read(gameport) >> shift;
 
 	do {
 		t--;
-		u = v; v = (inb(io) >> shift) & 3;
+		u = v; v = (gameport_read(gameport) >> shift) & 3;
 		if (~v & u & 1) {
 			data[0] |= (v >> 1) << i++;
-			p = t = (p - t) << 1;
+			t = strobe;
 		}
-	} while (i < JS_GR_LENGTH_GPP && t > 0);
+	} while (i < GRIP_LENGTH_GPP && t > 0);
 
 	__restore_flags(flags);
 
-	if (i < JS_GR_LENGTH_GPP) return -1;
+	if (i < GRIP_LENGTH_GPP) return -1;
 
-	for (i = 0; i < JS_GR_LENGTH_GPP && (data[0] & 0xfe4210) ^ 0x7c0000; i++)
-		data[0] = data[0] >> 1 | (data[0] & 1) << (JS_GR_LENGTH_GPP - 1);
+	for (i = 0; i < GRIP_LENGTH_GPP && (data[0] & 0xfe4210) ^ 0x7c0000; i++)
+		data[0] = data[0] >> 1 | (data[0] & 1) << (GRIP_LENGTH_GPP - 1);
 
-	return -(i == JS_GR_LENGTH_GPP);
+	return -(i == GRIP_LENGTH_GPP);
 }
 
 /*
- * js_gr_xt_read_packet() reads a Gravis Xterminator packet.
+ * grip_xt_read_packet() reads a Gravis Xterminator packet.
  */
 
-static int js_gr_xt_read_packet(int io, int shift, unsigned int *data)
+static int grip_xt_read_packet(struct gameport *gameport, int shift, unsigned int *data)
 {
 	unsigned int i, j, buf, crc;
 	unsigned char u, v, w;
 	unsigned long flags;
-	unsigned int t, p;
+	unsigned int t;
 	char status;
+
+	int strobe = gameport_time(GRIP_STROBE_XT);
 
 	data[0] = data[1] = data[2] = data[3] = 0;
 	status = buf = i = j = 0;
-	p = t = JS_GR_STROBE_XT;
-	p += JS_GR_STROBE_XT;
+	t = strobe;
 
 	__save_flags(flags);
 	__cli();
@@ -124,18 +124,17 @@ static int js_gr_xt_read_packet(int io, int shift, unsigned int *data)
 
 	do {
 		t--;
-		u = (inb(io) >> shift) & 3;
+		u = (gameport_read(io) >> shift) & 3;
 
 		if (u ^ v) {
 
 			if ((u ^ v) & 1) {
-				p = t = (p - t) << 2;
 				buf = (buf << 1) | (u >> 1);
+				t = strobe;
 				i++;
 			} else 
 
 			if ((((u ^ v) & (v ^ w)) >> 1) & ~(u | v | w) & 1) {
-				p = t = (p - t) << 2;
 				if (i == 20) {
 					crc = buf ^ (buf >> 7) ^ (buf >> 14);
 					if (!((crc ^ (0x25cb9e70 >> ((crc >> 2) & 0x1c))) & 0xf)) {
@@ -144,15 +143,15 @@ static int js_gr_xt_read_packet(int io, int shift, unsigned int *data)
 					}
 					j++;
 				}
+				t = strobe;
 				buf = 0;
 				i = 0;
 			}
-
 			w = v;
 			v = u;
 		}
 
-	} while (status != 0xf && i < JS_GR_MAX_BITS_XT && j < JS_GR_MAX_CHUNKS_XT && t > 0);
+	} while (status != 0xf && i < GRIP_MAX_BITS_XT && j < GRIP_MAX_CHUNKS_XT && t > 0);
 
 	__restore_flags(flags);
 
@@ -160,24 +159,27 @@ static int js_gr_xt_read_packet(int io, int shift, unsigned int *data)
 }
 
 /*
- * js_gr_read() reads and analyzes GrIP joystick data.
+ * grip_timer() repeatedly polls the joysticks and generates events.
  */
 
-static int js_gr_read(void *xinfo, int **axes, int **buttons)
+static void grip_timer(unsigned long private)
 {
-	struct js_gr_info *info = xinfo;
-	unsigned int data[JS_GR_LENGTH_XT];
+	struct grip *grip = (void*) private;;
+	unsigned int data[GRIP_LENGTH_XT];
 	int i;
 
-	for (i = 0; i < 2; i++)
-		switch (info->mode[i]) {
+	for (i = 0; i < 2; i++) {
 
-			case JS_GR_MODE_GPP:
+		dev = grip->dev + i;
 
-				if (js_gr_gpp_read_packet(info->io, (i << 1) + 4, data)) return -1;
+		switch (grip->mode[i]) {
 
-				axes[i][0] = ((data[0] >> 15) & 1) - ((data[0] >> 16) & 1);
-				axes[i][1] = ((data[0] >> 13) & 1) - ((data[0] >> 12) & 1);
+			case GRIP_MODE_GPP:
+
+				if (grip_gpp_read_packet(grip->gameport, (i << 1) + 4, data)) return -1;
+
+				input_report_abs(dev, ABS_X, ((data[0] >> 15) & 1) - ((data[0] >> 16) & 1));
+				input_report_abs(dev, ABS_X, ((data[0] >> 13) & 1) - ((data[0] >> 12) & 1));
 
 				data[0] = ((data[0] >> 6) & 0x37) | (data[0] & 0x08) | ((data[0] << 1) & 0x40) |
 				       ((data[0] << 5) & 0x80) | ((data[0] << 8) & 0x300);
@@ -186,9 +188,9 @@ static int js_gr_read(void *xinfo, int **axes, int **buttons)
 
 				break;
 
-			case JS_GR_MODE_XT:
+			case GRIP_MODE_XT:
 
-				if (js_gr_xt_read_packet(info->io, (i << 1) + 4, data)) return -1;
+				if (grip_xt_read_packet(grip->gameport, (i << 1) + 4, data)) return -1;
 
 				axes[i][0] =       (data[0] >> 2) & 0x3f;
 				axes[i][1] = 63 - ((data[0] >> 8) & 0x3f);
@@ -205,9 +207,9 @@ static int js_gr_read(void *xinfo, int **axes, int **buttons)
 
 				break;
 
-			case JS_GR_MODE_BD:
+			case GRIP_MODE_BD:
 
-				if (js_gr_xt_read_packet(info->io, (i << 1) + 4, data)) return -1;
+				if (grip_xt_read_packet(grip->gameport, (i << 1) + 4, data)) return -1;
 
 				axes[i][0] =       (data[0] >> 2) & 0x3f;
 				axes[i][1] = 63 - ((data[0] >> 8) & 0x3f);
@@ -225,43 +227,43 @@ static int js_gr_read(void *xinfo, int **axes, int **buttons)
 				break;
 
 		}
-
+	}
 
 	return 0;
 }
 
 /*
- * js_gr_open() is a callback from the file open routine.
+ * grip_open() is a callback from the file open routine.
  */
 
-static int js_gr_open(struct js_dev *jd)
+static int grip_open(struct js_dev *jd)
 {
 	MOD_INC_USE_COUNT;
 	return 0;
 }
 
 /*
- * js_gr_close() is a callback from the file release routine.
+ * grip_close() is a callback from the file release routine.
  */
 
-static int js_gr_close(struct js_dev *jd)
+static int grip_close(struct js_dev *jd)
 {
 	MOD_DEC_USE_COUNT;
 	return 0;
 }
 
 /*
- * js_gr_init_corr() initializes correction values of
+ * grip_init_corr() initializes correction values of
  * GrIP joysticks.
  */
 
-static void __init js_gr_init_corr(int mode, struct js_corr *corr)
+static void __init grip_init_corr(int mode, struct js_corr *corr)
 {
 	int i;
 
 	switch (mode) {
 
-		case JS_GR_MODE_GPP:
+		case GRIP_MODE_GPP:
 
 			for (i = 0; i < 2; i++) {
 				corr[i].type = JS_CORR_BROKEN;
@@ -274,7 +276,7 @@ static void __init js_gr_init_corr(int mode, struct js_corr *corr)
 
 			break;
 
-		case JS_GR_MODE_XT:
+		case GRIP_MODE_XT:
 
 			for (i = 0; i < 5; i++) {
 				corr[i].type = JS_CORR_BROKEN;
@@ -296,7 +298,7 @@ static void __init js_gr_init_corr(int mode, struct js_corr *corr)
 
 			break;
 
-		case JS_GR_MODE_BD:
+		case GRIP_MODE_BD:
 
 			for (i = 0; i < 3; i++) {
 				corr[i].type = JS_CORR_BROKEN;
@@ -322,46 +324,46 @@ static void __init js_gr_init_corr(int mode, struct js_corr *corr)
 }
 
 /*
- * js_gr_probe() probes for GrIP joysticks.
+ * grip_probe() probes for GrIP joysticks.
  */
 
-static struct js_port __init *js_gr_probe(int io, struct js_port *port)
+static struct js_port __init *grip_probe(int io, struct js_port *port)
 {
-	struct js_gr_info info;
+	struct grip grip;
 	char *names[] = { NULL, "Gravis GamePad Pro", "Gravis Xterminator", "Gravis Blackhawk Digital"};
 	char axes[] = { 0, 2, 9, 5};
 	char buttons[] = { 0, 10, 11, 5};
-	unsigned int data[JS_GR_LENGTH_XT];
+	unsigned int data[GRIP_LENGTH_XT];
 	int i;
 
 	if (check_region(io, 1)) return port;
 
-	info.mode[0] = info.mode[1] = 0;
+	grip.mode[0] = grip.mode[1] = 0;
 
 	for (i = 0; i < 2; i++) {
-		if (!js_gr_gpp_read_packet(io, (i << 1) + 4, data)) info.mode[i] = JS_GR_MODE_GPP;
-		if (!js_gr_xt_read_packet(io, (i << 1) + 4, data)) {
+		if (!grip_gpp_read_packet(io, (i << 1) + 4, data)) grip.mode[i] = GRIP_MODE_GPP;
+		if (!grip_xt_read_packet(io, (i << 1) + 4, data)) {
 			if ((data[3] & 7) == 7)
-				info.mode[i] = JS_GR_MODE_XT;
+				grip.mode[i] = GRIP_MODE_XT;
 			if ((data[3] & 7) == 0)
-				info.mode[i] = JS_GR_MODE_BD;
+				grip.mode[i] = GRIP_MODE_BD;
 		}
 	}
 
-	if (!info.mode[0] && !info.mode[1]) return port;
+	if (!grip.mode[0] && !grip.mode[1]) return port;
 
-	info.io = io;
+	grip.io = io;
 
 	request_region(io, 1, "joystick (gravis)");
-	port = js_register_port(port, &info, 2, sizeof(struct js_gr_info), js_gr_read);
+	port = js_register_port(port, &grip, 2, sizeof(struct grip), grip_read);
 
 	for (i = 0; i < 2; i++)
-		if (info.mode[i]) {
+		if (grip.mode[i]) {
 			printk(KERN_INFO "js%d: %s at %#x\n",
-				js_register_device(port, i, axes[info.mode[i]], buttons[info.mode[i]],
-					names[info.mode[i]], js_gr_open, js_gr_close),
-				names[info.mode[i]], io);
-			js_gr_init_corr(info.mode[i], port->corr[i]);
+				js_register_device(port, i, axes[grip.mode[i]], buttons[grip.mode[i]],
+					names[grip.mode[i]], grip_open, grip_close),
+				names[grip.mode[i]], io);
+			grip_init_corr(grip.mode[i], port->corr[i]);
 		}
 
 	return port;
@@ -370,13 +372,13 @@ static struct js_port __init *js_gr_probe(int io, struct js_port *port)
 #ifdef MODULE
 int init_module(void)
 #else
-int __init js_gr_init(void)
+int __init grip_init(void)
 #endif
 {
 	int *p;
 
-	for (p = js_gr_port_list; *p; p++) js_gr_port = js_gr_probe(*p, js_gr_port);
-	if (js_gr_port) return 0;
+	for (p = grip_port_list; *p; p++) grip_port = grip_probe(*p, grip_port);
+	if (grip_port) return 0;
 
 #ifdef MODULE
 	printk(KERN_WARNING "joy-gravis: no joysticks found\n");
@@ -389,15 +391,15 @@ int __init js_gr_init(void)
 void cleanup_module(void)
 {
 	int i;
-	struct js_gr_info *info;
+	struct grip *grip;
 
-	while (js_gr_port) {
-		for (i = 0; i < js_gr_port->ndevs; i++)
-			if (js_gr_port->devs[i])
-				js_unregister_device(js_gr_port->devs[i]);
-		info = js_gr_port->info;
-		release_region(info->io, 1);
-		js_gr_port = js_unregister_port(js_gr_port);
+	while (grip_port) {
+		for (i = 0; i < grip_port->ndevs; i++)
+			if (grip_port->devs[i])
+				js_unregister_device(grip_port->devs[i]);
+		grip = grip_port->grip;
+		release_region(grip->io, 1);
+		grip_port = js_unregister_port(grip_port);
 	}
 }
 #endif
