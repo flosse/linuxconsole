d833accb059c34867b8502422c014df5f747e431
Vojtech Pavlik <vojtech@suse.cz>
2000-05-29 17:53:43 +0000

    db9.c compiles, needs testing.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@317 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/drivers/input/db9.c
+++ ruby/linux/drivers/input/db9.c
@@ -81,22 +81,22 @@ static int db9_2[] __initdata = { -1, 0 };
 static int db9_3[] __initdata = { -1, 0 };
 
 struct db9 {
-	struct input_device dev[2];
+	struct input_dev dev[2];
 	struct timer_list timer;
 	struct pardevice *pd;	
 	int mode;
 	int used;
 };
 
-static struct db9 db9_base[3];
+static struct db9 *db9_base[3];
 
-static unsigned char db9_multi_btn = { BTN_TRIGGER, BTN_THUMB };
-static unsigned char db9_genesis_btn = { BTN_START, BTN_A, BTN_B, BTN_C, BTN_X, BTN_Y, BTN_Z, BTN_MODE };
-static unsigned char db9_cd32_btn = { BTN_A, BTN_B, BTN_C, BTN_X, BTN_Y, BTN_Z, BTN_TL, BTN_TR, BTN_START };
+static short db9_multi_btn[] = { BTN_TRIGGER, BTN_THUMB };
+static short db9_genesis_btn[] = { BTN_START, BTN_A, BTN_B, BTN_C, BTN_X, BTN_Y, BTN_Z, BTN_MODE };
+static short db9_cd32_btn[] = { BTN_A, BTN_B, BTN_C, BTN_X, BTN_Y, BTN_Z, BTN_TL, BTN_TR, BTN_START };
 
 static char db9_buttons[DB9_MAX_PAD] = { 0, 1, 2, 4, 0, 6, 8, 8, 1, 1, 7 };
-static char *db9_btn[DB9_MAX_PAD] = { db9_multi_btn, db9_multi_btn, db9_genesis_btn, NULL, db9_genesis_btn, db9_genesis_btn,
-				     db9_cd32_btn, db9_multi_btn, db9_multi_btn, db9_cd32_btn };
+static short *db9_btn[DB9_MAX_PAD] = { db9_multi_btn, db9_multi_btn, db9_genesis_btn, NULL, db9_genesis_btn,
+					db9_genesis_btn, db9_cd32_btn, db9_multi_btn, db9_multi_btn, db9_cd32_btn };
 static char *db9_name[DB9_MAX_PAD] = { NULL, "Multisystem joystick", "Multisystem joystick (2 fire)", "Genesis pad",
 				      NULL, "Genesis 5 pad", "Genesis 6 pad", "Saturn pad", "Multisystem (0.8.0.2) joystick",
 				     "Multisystem (0.8.0.2-dual) joystick", "Amiga CD-32 pad" };
@@ -105,6 +105,7 @@ static void db9_timer(unsigned long private)
 {
 	struct db9 *db9 = (void *) private;
 	struct parport *port = db9->pd->port;
+	struct input_dev *dev = db9->dev;
 	int data, i;
 
 	switch(db9->mode) {
@@ -146,7 +147,7 @@ static void db9_timer(unsigned long private)
 
 		case DB9_GENESIS_PAD:
 
-			parport_write_control(DB9_NOSELECT, port);
+			parport_write_control(port, DB9_NOSELECT);
 			data = parport_read_data(port);
 
 			input_report_abs(dev, ABS_X, (data&DB9_DOWN ?0:1) - (data&DB9_UP  ?0:1));
@@ -154,7 +155,7 @@ static void db9_timer(unsigned long private)
 			input_report_key(dev, BTN_B, ~data&DB9_FIRE1);
 			input_report_key(dev, BTN_C, ~data&DB9_FIRE2);
 
-			parport_write_control(DB9_NORMAL, port);
+			parport_write_control(port, DB9_NORMAL);
 			data=parport_read_data(port);
 
 			input_report_key(dev, BTN_A,     ~data&DB9_FIRE1);
@@ -163,7 +164,7 @@ static void db9_timer(unsigned long private)
 
 		case DB9_GENESIS5_PAD:
 
-			parport_write_control(DB9_NOSELECT, port);
+			parport_write_control(port, DB9_NOSELECT);
 			data=parport_read_data(port);
 
 			input_report_abs(dev, ABS_X, (data&DB9_DOWN ?0:1) - (data&DB9_UP  ?0:1));
@@ -171,7 +172,7 @@ static void db9_timer(unsigned long private)
 			input_report_key(dev, BTN_B, ~data&DB9_FIRE1);
 			input_report_key(dev, BTN_C, ~data&DB9_FIRE2);
 
-			parport_write_control(DB9_NORMAL, port);
+			parport_write_control(port, DB9_NORMAL);
 			data=parport_read_data(port);
 
 			input_report_key(dev, BTN_A,     ~data&DB9_FIRE1);
@@ -182,7 +183,7 @@ static void db9_timer(unsigned long private)
 
 		case DB9_GENESIS6_PAD:
 
-			parport_write_control(DB9_NOSELECT, port); /* 1 */
+			parport_write_control(port, DB9_NOSELECT); /* 1 */
 			udelay(DB9_GENESIS6_DELAY);
 			data=parport_read_data(port);
 
@@ -191,36 +192,36 @@ static void db9_timer(unsigned long private)
 			input_report_key(dev, BTN_B, ~data&DB9_FIRE1);
 			input_report_key(dev, BTN_C, ~data&DB9_FIRE2);
 
-			parport_write_control(DB9_NORMAL, port);
+			parport_write_control(port, DB9_NORMAL);
 			udelay(DB9_GENESIS6_DELAY);
 			data=parport_read_data(port);
 
 			input_report_key(dev, BTN_A, ~data&DB9_FIRE1);
 			input_report_key(dev, BTN_X, ~data&DB9_FIRE2);
 
-			parport_write_control(DB9_NOSELECT, port); /* 2 */
+			parport_write_control(port, DB9_NOSELECT); /* 2 */
 			udelay(DB9_GENESIS6_DELAY);
-			parport_write_control(DB9_NORMAL, port);
+			parport_write_control(port, DB9_NORMAL);
 			udelay(DB9_GENESIS6_DELAY);
-			parport_write_control(DB9_NOSELECT, port); /* 3 */
+			parport_write_control(port, DB9_NOSELECT); /* 3 */
 			udelay(DB9_GENESIS6_DELAY);
 			data=parport_read_data(port);
 
 			input_report_key(dev, BTN_Y,     ~data&DB9_LEFT);
 			input_report_key(dev, BTN_Z,     ~data&DB9_DOWN);
 			input_report_key(dev, BTN_MODE,  ~data&DB9_UP);
-			input_report_key(dev, BTN_START  ~data&DB9_RIGHT);
+			input_report_key(dev, BTN_START, ~data&DB9_RIGHT);
 
-			parport_write_control(DB9_NORMAL, port);
+			parport_write_control(port, DB9_NORMAL);
 			udelay(DB9_GENESIS6_DELAY);
-			parport_write_control(DB9_NOSELECT, port); /* 4 */
+			parport_write_control(port, DB9_NOSELECT); /* 4 */
 			udelay(DB9_GENESIS6_DELAY);
-			parport_write_control(DB9_NORMAL, port);
+			parport_write_control(port, DB9_NORMAL);
 			break;
 
 		case DB9_SATURN_PAD:
 
-			parport_write_control(DB9_SATURN0, port);
+			parport_write_control(port, DB9_SATURN0);
 			data = parport_read_data(port);
 
 			input_report_key(dev, BTN_Y, ~data&DB9_LEFT);
@@ -228,13 +229,13 @@ static void db9_timer(unsigned long private)
 			input_report_key(dev, BTN_TL,~data&DB9_UP);
 			input_report_key(dev, BTN_TR,~data&DB9_RIGHT);
 
-			parport_write_control(DB9_SATURN2, port);
+			parport_write_control(port, DB9_SATURN2);
 			data = parport_read_data(port);
 
 			input_report_abs(dev, ABS_X, (data&DB9_DOWN ?0:1) - (data&DB9_UP  ?0:1));
 			input_report_abs(dev, ABS_Y, (data&DB9_RIGHT?0:1) - (data&DB9_LEFT?0:1));
 			
-			parport_write_control(DB9_NORMAL, port);
+			parport_write_control(port, DB9_NORMAL);
 			data = parport_read_data(port);
 
 			input_report_key(dev, BTN_A, ~data&DB9_LEFT);
@@ -250,18 +251,16 @@ static void db9_timer(unsigned long private)
 			input_report_abs(dev, ABS_X, (data&DB9_DOWN ?0:1) - (data&DB9_UP  ?0:1));
 			input_report_abs(dev, ABS_Y, (data&DB9_RIGHT?0:1) - (data&DB9_LEFT?0:1));
 
-			parport_write_control(0x0a, port); 
+			parport_write_control(port, 0x0a); 
 
-				int i;
-				for (i = 0; i < 7; i++) { 
-					data = parport_read_data(port);
-					parport_write_control(0x02, port); 
-					parport_write_control(0x0a, port); 
-					input_report_key(dev, db9_cd32_btn[i], ~data&DB9_FIRE2);
+			for (i = 0; i < 7; i++) { 
+				data = parport_read_data(port);
+				parport_write_control(port, 0x02); 
+				parport_write_control(port, 0x0a); 
+				input_report_key(dev, db9_cd32_btn[i], ~data&DB9_FIRE2);
 				}
-			}
 
-			parport_write_control(0x00, port); 
+			parport_write_control(port, 0x00); 
 			break;
 		}
 
@@ -275,31 +274,33 @@ static int db9_open(struct input_dev *dev)
 
 	if (!db9->used++) {
 		if (parport_claim(db9->pd)) return -EBUSY;
-		parport_write_data(0xff, port);
+		parport_write_data(port, 0xff);
 		parport_data_reverse(port);
-		parport_write_control(DB9_NORMAL, port);
+		parport_write_control(port, DB9_NORMAL);
 		mod_timer(&db9->timer, jiffies + DB9_REFRESH_TIME);
 	}
+
 	return 0;
 }
 
 static void db9_close(struct input_dev *dev)
 {
 	struct db9 *db9 = dev->private;
+	struct parport *port = db9->pd->port;
+
 	if (!--db9->used) {
 		del_timer(&db9->timer);
 		parport_write_control(port, 0x00);
 		parport_data_forward(port);
 		parport_release(db9->pd);
 	}
-	return 0;
 }
 
 static struct db9 __init *db9_probe(int *config)
 {
 	struct db9 *db9;
 	struct parport *pp;
-	int i;
+	int i, j;
 
 	if (config[0] < 0)
 		return NULL;
@@ -316,8 +317,8 @@ static struct db9 __init *db9_probe(int *config)
 		return NULL;
 	}
 
-	if (!(pp->modes & (PARPORT_MODE_PCPS2 | PARPORT_MODE_PCECPPS2)) && db9->mode != DB9_MULTI_0802) {
-		printk(KERN_ERR "js-db9: specified parport is not bidirectional\n");
+	if (!(pp->modes & PARPORT_MODE_TRISTATE) && config[1] != DB9_MULTI_0802) {
+		printk(KERN_ERR "db9.c: specified parport is not bidirectional\n");
 		return NULL;
 	}
 	
@@ -364,7 +365,7 @@ static struct db9 __init *db9_probe(int *config)
 			db9->dev[i].number, db9_name[db9->mode], db9->pd->port->name);
 	}
 
-	return pd;
+	return db9;
 }
 
 #ifndef MODULE
@@ -412,7 +413,7 @@ void __exit db9_exit(void)
 
 	for (i = 0; i < 3; i++) 
 		if (db9_base[i]) {
-			for (j = 0; j < 1 + (db9->mode == DB9_MULTI_0802_2); j++)
+			for (j = 0; j < 1 + (db9_base[i]->mode == DB9_MULTI_0802_2); j++)
 				input_unregister_device(db9_base[i]->dev + j);
 		parport_unregister_device(db9_base[i]->pd);
 	}
