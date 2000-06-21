ee1db5a60a7b5cb7d29ccbc18823077dea7f7cf4
James Simmons <jsimmons@users.sf.net>
2000-06-21 23:55:14 +0000

    removed some of the currcon removal
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@429 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/include/linux/selection.h
+++ ruby/linux/include/linux/selection.h
@@ -9,6 +9,7 @@
 
 #include <linux/vt_buffer.h>
 
+struct vc_data *vc;
 extern int sel_cons;
 
 extern void clear_selection(void);
@@ -18,10 +19,10 @@ extern int sel_loadlut(const unsigned long arg);
 extern int mouse_reporting(void);
 extern void mouse_report(struct tty_struct * tty, int butt, int mrx, int mry);
 
-#define video_num_columns	(vc->vc_cols)
-#define video_num_lines		(vc->vc_rows)
-#define video_size_row		(vc->vc_size_row)
-#define can_do_color		(vc->vc_can_do_color)
+#define video_num_columns	(vc_cons[currcons].d->vc_cols)
+#define video_num_lines		(vc_cons[currcons].d->vc_rows)
+#define video_size_row		(vc_cons[currcons].d->vc_size_row)
+#define can_do_color		(vc_cons[currcons].d->vc_can_do_color)
 
 extern int console_blanked;
 
@@ -30,15 +31,15 @@ extern int default_red[];
 extern int default_grn[];
 extern int default_blu[];
 
-extern unsigned short *screen_pos(int currcons, int w_offset, int viewed);
-extern u16 screen_glyph(int currcons, int offset);
-extern void complement_pos(int currcons, int offset);
-extern void invert_screen(int currcons, int offset, int count, int shift);
+extern unsigned short *screen_pos(struct vc_data *vc, int w_offset, int viewed);
+extern u16 screen_glyph(struct vc_data *vc, int offset);
+extern void complement_pos(struct vc_data *vc, int offset);
+extern void invert_screen(struct vc_data *vc, int offset, int count, int shift);
 
-extern void getconsxy(int currcons, char *p);
-extern void putconsxy(int currcons, char *p);
+extern void getconsxy(struct vc_data *vc, char *p);
+extern void putconsxy(struct vc_data *vc, char *p);
 
-extern u16 vcs_scr_readw(int currcons, const u16 *org);
-extern void vcs_scr_writew(int currcons, u16 val, u16 *org);
+extern u16 vcs_scr_readw(struct vc_data *vc, const u16 *org);
+extern void vcs_scr_writew(struct vc_data *vc, u16 val, u16 *org);
 
 #endif
