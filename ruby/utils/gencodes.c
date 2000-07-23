a6b1fbee639dd50e0218c426c4d2524ee485ad65
Vojtech Pavlik <vojtech@suse.cz>
2000-07-23 23:27:23 +0000

    Adding simplistic tools to generate scancode conversion tables.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@577 81ba4a36-902d-0410-9329-aea0610344cf

---
+++ ruby/utils/gencodes.c
@@ -0,0 +1,47 @@
+#define from	code
+#define to	xt
+
+#include "scancodes.h"
+
+void main(void)
+{
+
+	int i, j;
+	unsigned int keycode[4096];
+
+	memset(keycode, 0, 4096 * sizeof(int));
+
+	for (i = 0; scancodes[i].code; i++) {
+		if (scancodes[i].from && scancodes[i].to) {
+			if (!keycode[scancodes[i].from])
+				keycode[scancodes[i].from] = scancodes[i].to;
+			else
+				printf("Clash: %3d and %3d on 0x%02x\n",
+					scancodes[i].to, keycode[scancodes[i].from], scancodes[i].from);
+		}
+	}
+
+	for (j = 4095; j > 0; j--)
+		if (keycode[j]) break;
+
+	for (i = 0; i <= j; i++) {
+		if (keycode[i])
+			printf("%3d,", keycode[i]);
+		else
+			printf("  0,");
+		if ((i & 0xf) == 0xf)
+			printf("\n");
+	}
+#if 0
+	for (i = 0; i <= j; i++) {
+		if (!keycode[i])
+			printf("%03x ", i);
+		else
+			printf("    ");
+		if ((i & 0xf) == 0xf)
+			printf("\n");
+	}
+#endif
+
+	printf("\n");
+}
