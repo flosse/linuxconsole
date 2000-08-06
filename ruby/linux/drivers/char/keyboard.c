c12313cabd4968a6d64ec25b5adea8f727438f4a
James Simmons <jsimmons@users.sf.net>
2000-08-06 23:30:11 +0000

    Commented out 3 finger salute until I figure out why it crashes the OS.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@629 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/drivers/char/keyboard.c
+++ ruby/linux/drivers/char/keyboard.c
@@ -441,8 +441,8 @@ static void fn_boot_it(struct tty_struct *tty)
 {
 	struct vc_data *vc = (struct vc_data *) tty->driver_data;
 
-	if (vc->display_fg == admin_vt)	
-		ctrl_alt_del();
+	if (vc->display_fg == admin_vt)
+		/* ctrl_alt_del(); */
 }
 
 static void fn_null(struct tty_struct *tty)
