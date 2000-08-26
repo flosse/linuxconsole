d0125f718efb197042a7394d6d68213abc1468f2
James Simmons <jsimmons@users.sf.net>
2000-08-26 13:02:26 +0000

    Adapted to new fbdev API. This acts as a bridge to the new fbdev api from the old one.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@706 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/drivers/video/fbgen.c
+++ ruby/linux/drivers/video/fbgen.c
@@ -1,6 +1,8 @@
 /*
  * linux/drivers/video/fbgen.c -- Generic routines for frame buffer devices
  *
+ *  Modified to new API May 2000 by James Simmons
+ *
  *  Created 3 Jan 1998 by Geert Uytterhoeven
  *
  * This file is subject to the terms and conditions of the GNU General Public
@@ -19,86 +21,76 @@
 
 #include "fbcon.h"
 
-static int currcon = 0;
-
-
 /* ---- `Generic' versions of the frame buffer device operations ----------- */
-
 
     /*
      *  Get the Fixed Part of the Display
      */
 
-int fbgen_get_fix(struct fb_fix_screeninfo *fix, int con, struct fb_info *info)
+int fbgen_get_fix(struct fb_fix_screeninfo *fix, struct fb_info *info)
 {
-    struct fb_info_gen *info2 = (struct fb_info_gen *)info;
-    struct fbgen_hwswitch *fbhw = info2->fbhw;
-    char par[info2->parsize];
-
-    if (con == -1)
-	fbhw->get_par(&par, info2);
-    else {
-	int err;
-
-	if ((err = fbhw->decode_var(&fb_display[con].var, &par, info2)))
-	    return err;
-    }
-    memset(fix, 0, sizeof(struct fb_fix_screeninfo));
-    return fbhw->encode_fix(fix, &par, info2);
+    *fix = info->fix;	    
+    return 0;
 }
-
 
     /*
      *  Get the User Defined Part of the Display
      */
 
-int fbgen_get_var(struct fb_var_screeninfo *var, int con, struct fb_info *info)
+int fbgen_get_var(struct fb_var_screeninfo *var, struct fb_info *info)
 {
-    struct fb_info_gen *info2 = (struct fb_info_gen *)info;
-    struct fbgen_hwswitch *fbhw = info2->fbhw;
-    char par[info2->parsize];
-
-    if (con == -1) {
-	fbhw->get_par(&par, info2);
-	fbhw->encode_var(var, &par, info2);
-    } else
-	*var = fb_display[con].var;
+    *var = info->var;
     return 0;
 }
-
 
     /*
      *  Set the User Defined Part of the Display
      */
 
-int fbgen_set_var(struct fb_var_screeninfo *var, int con, struct fb_info *info)
+int fbgen_set_var(struct fb_var_screeninfo *var, struct fb_info *info)
 {
-    struct fb_info_gen *info2 = (struct fb_info_gen *)info;
-    int err;
-    int oldxres, oldyres, oldbpp, oldxres_virtual, oldyres_virtual, oldyoffset;
-
-    if ((err = fbgen_do_set_var(var, con == currcon, info2)))
-	return err;
-    if ((var->activate & FB_ACTIVATE_MASK) == FB_ACTIVATE_NOW) {
-	oldxres = fb_display[con].var.xres;
-	oldyres = fb_display[con].var.yres;
-	oldxres_virtual = fb_display[con].var.xres_virtual;
-	oldyres_virtual = fb_display[con].var.yres_virtual;
-	oldbpp = fb_display[con].var.bits_per_pixel;
-	oldyoffset = fb_display[con].var.yoffset;
-	fb_display[con].var = *var;
-	if (oldxres != var->xres || oldyres != var->yres ||
-	    oldxres_virtual != var->xres_virtual ||
-	    oldyres_virtual != var->yres_virtual ||
-	    oldbpp != var->bits_per_pixel ||
-	    oldyoffset != var->yoffset) {
-	    fbgen_set_disp(con, info2);
-	    if ((err = fb_alloc_cmap(&fb_display[con].cmap, 0, 0)))
+    int err, oldbpp;
+	
+    if (memcmp(info->var, var, sizeof(var)) {	
+	if ((err = info->fbops->fb_check_var(var, info)))
 		return err;
-	    fbgen_install_cmap(con, info2);
+
+    	if ((var->activate & FB_ACTIVATE_MASK) == FB_ACTIVATE_NOW) {
+		oldbpp = info->var.bits_per_pixel;
+	    	info->fbops->fb_set_par(info); 
+ 
+	    	if (oldbpp != var->bits_per_pixel) {
+			if ((err = fb_set_cmap(&info->cmap, 1, info)))
+            			return err;
+      	    	}
 	}
+	var->activate = 0;
     }
-    var->activate = 0;
+    return 0;
+}
+
+    /*
+     *  Blank the screen
+     */
+
+int fbgen_blank(int blank, struct fb_info *info)
+{
+    struct fb_cmap cmap;
+    u16 black[16];
+
+    if (info->fbops->fb_blank && !info->fbops->fb_blank(blank, info))
+        return 0;
+    if (blank) {
+        memset(black, 0, 16*sizeof(u16));
+        cmap.red = black;
+        cmap.green = black;
+        cmap.blue = black;
+        cmap.transp = NULL;
+        cmap.start = 0;
+        cmap.len = 16;
+        fb_set_cmap(&cmap, 1, info);
+        return 0;
+    }
     return 0;
 }
 
@@ -108,136 +100,110 @@ int fbgen_set_var(struct fb_var_screeninfo *var, int con, struct fb_info *info)
      *  This call looks only at xoffset, yoffset and the FB_VMODE_YWRAP flag
      */
 
-int fbgen_pan_display(struct fb_var_screeninfo *var, int con,
-		      struct fb_info *info)
+int fbgen_pan_display(struct fb_var_screeninfo *var, struct fb_info *info) 
 {
-    struct fb_info_gen *info2 = (struct fb_info_gen *)info;
-    struct fbgen_hwswitch *fbhw = info2->fbhw;
-    int xoffset = var->xoffset;
-    int yoffset = var->yoffset;
     int err;
 
-    if (xoffset < 0 ||
-	xoffset+fb_display[con].var.xres > fb_display[con].var.xres_virtual ||
-	yoffset < 0 ||
-	yoffset+fb_display[con].var.yres > fb_display[con].var.yres_virtual)
+    if (var->xoffset + info->var.xres > info->var.xres_virtual ||
+	var->yoffset + info->var.yres > info->var.yres_virtual || 
+	var->xoffset < 0 || var->yoffset < 0)
 	return -EINVAL;
-    if (con == currcon) {
-	if (fbhw->pan_display) {
-	    if ((err = fbhw->pan_display(var, info2)))
+    if (info->fbops->fb_pan_display) {
+    	if ((err = info->fbops->fb_pan_display(var, info)))
 		return err;
 	} else
 	    return -EINVAL;
     }
-    fb_display[con].var.xoffset = var->xoffset;
-    fb_display[con].var.yoffset = var->yoffset;
+    info->var.xoffset = var->xoffset;
+    info->var.yoffset = var->yoffset;
     if (var->vmode & FB_VMODE_YWRAP)
-	fb_display[con].var.vmode |= FB_VMODE_YWRAP;
+	info->var.vmode |= FB_VMODE_YWRAP;
     else
-	fb_display[con].var.vmode &= ~FB_VMODE_YWRAP;
-
+	info->var.vmode &= ~FB_VMODE_YWRAP;
     return 0;
 }
 
-/* ---- Helper functions --------------------------------------------------- */
-
-
-    /*
-     *  Change the video mode
-     */
-
-int fbgen_do_set_var(struct fb_var_screeninfo *var, int isactive,
-		     struct fb_info_gen *info)
+void fbgen_set_disp(int con, struct fb_info *info)
 {
-    struct fbgen_hwswitch *fbhw = info->fbhw;
-    int err, activate;
-    char par[info->parsize];
-
-    if ((err = fbhw->decode_var(var, &par, info)))
-	return err;
-    activate = var->activate;
-    if (((var->activate & FB_ACTIVATE_MASK) == FB_ACTIVATE_NOW) && isactive)
-	fbhw->set_par(&par, info);
-    fbhw->encode_var(var, &par, info);
-    var->activate = activate;
-    return 0;
-}
-
-
-void fbgen_set_disp(int con, struct fb_info_gen *info)
-{
-    struct fbgen_hwswitch *fbhw = info->fbhw;
-    struct fb_fix_screeninfo fix;
-    char par[info->parsize];
     struct display *display;
 
     if (con >= 0)
 	display = &fb_display[con];
     else
-	display = info->info.disp;	/* used during initialization */
+	display = info->disp;	/* used during initialization */
 
-    if (con == -1)
-	fbhw->get_par(&par, info);
-    else
-	fbhw->decode_var(&fb_display[con].var, &par, info);
-    memset(&fix, 0, sizeof(struct fb_fix_screeninfo));
-    fbhw->encode_fix(&fix, &par, info);
-
-    display->visual = fix.visual;
-    display->type = fix.type;
-    display->type_aux = fix.type_aux;
-    display->ypanstep = fix.ypanstep;
-    display->ywrapstep = fix.ywrapstep;
-    display->line_length = fix.line_length;
-    if (info->fbhw->blank || fix.visual == FB_VISUAL_PSEUDOCOLOR ||
-	fix.visual == FB_VISUAL_DIRECTCOLOR)
+    display->screen_base = info->screen_base;	
+    display->visual = info->fix.visual;
+    display->type = info->fix.type;
+    display->type_aux = info->fix.type_aux;
+    display->ypanstep = info->fix.ypanstep;
+    display->ywrapstep = info->fix.ywrapstep;
+    display->line_length = info->fix.line_length;
+    if (info->fbops->fb_blank || info->fix.visual == FB_VISUAL_PSEUDOCOLOR ||
+	info->fix.visual == FB_VISUAL_DIRECTCOLOR)
 	display->can_soft_blank = 1;
     else
 	display->can_soft_blank = 0;
-    fbhw->set_disp(&par, display, info);
-#if 0 /* FIXME: generic inverse is not supported yet */
-    display->inverse = (fix.visual == FB_VISUAL_MONO01 ? !inverse : inverse);
-#else
-    display->inverse = fix.visual == FB_VISUAL_MONO01;
+
+    switch (info->var.bits_per_pixel) {
+#ifdef FBCON_HAS_MFB
+	case 1:
+            display->dispsw = &fbcon_mfb;
+            break;
 #endif
+#ifdef FBCON_HAS_CFB2
+        case 2:
+            display->dispsw = &fbcon_cfb2;
+            break;
+#endif
+#ifdef FBCON_HAS_CFB4
+        case 4:
+            display->dispsw = &fbcon_cfb4;
+            break;
+#endif
+#ifdef FBCON_HAS_CFB8
+        case 8:
+            display->dispsw = &fbcon_cfb8;
+            break;
+#endif
+#ifdef FBCON_HAS_CFB16
+        case 16:
+            display->dispsw = &fbcon_cfb16;
+            display->dispsw_data = info->pseudo_palette;
+            break;
+#endif
+#ifdef FBCON_HAS_CFB24
+        case 24:
+            display->dispsw = &fbcon_cfb24;
+            display->dispsw_data = info->pseudo_palette;
+            break;
+#endif
+#ifdef FBCON_HAS_CFB32
+        case 32:
+            display->dispsw = &fbcon_cfb32;
+            display->dispsw_data = info->pseudo_palette;
+            break;
+#endif
+        default:
+            display->dispsw = &fbcon_dummy;
+            break;
+        }
 }
-
-
-    /*
-     *  Install the current colormap
-     */
-
-void fbgen_install_cmap(int con, struct fb_info_gen *info)
-{
-    struct fbgen_hwswitch *fbhw = info->fbhw;
-    if (con != currcon)
-	return;
-    if (fb_display[con].cmap.len)
-	fb_set_cmap(&fb_display[con].cmap, 1, &info->info);
-    else {
-	int size = fb_display[con].var.bits_per_pixel == 16 ? 64 : 256;
-	fb_set_cmap(fb_default_cmap(size), 1, &info->info);
-    }
-}
-
 
     /*
      *  Update the `var' structure (called by fbcon.c)
      */
 
-int fbgen_update_var(int con, struct fb_info *info)
+int fbgen_update_var(struct fb_info *info)
 {
-    struct fb_info_gen *info2 = (struct fb_info_gen *)info;
-    struct fbgen_hwswitch *fbhw = info2->fbhw;
     int err;
 
-    if (fbhw->pan_display) {
-        if ((err = fbhw->pan_display(&fb_display[con].var, info2)))
+    if (info->fbops->fb_pan_display) {
+        if ((err = info->fbops->fb_pan_display(&info->var, info)))
             return err;
     }
     return 0;
 }
-
 
     /*
      *  Switch to a different virtual console
@@ -245,43 +211,21 @@ int fbgen_update_var(int con, struct fb_info *info)
 
 int fbgen_switch(int con, struct fb_info *info)
 {
-    struct fb_info_gen *info2 = (struct fb_info_gen *)info;
-    struct fbgen_hwswitch *fbhw = info2->fbhw;
+    struct display *prev = &fb_display[last_console];
+    struct display *new = &fb_display[con];
 
-    /* Do we have to save the colormap ? */
-    if (fb_display[currcon].cmap.len)
-	fb_get_cmap(&fb_display[currcon].cmap, 1, fbhw->getcolreg,
-		    &info2->info);
-    fbgen_do_set_var(&fb_display[con].var, 1, info2);
-    currcon = con;
-    /* Install new colormap */
-    fbgen_install_cmap(con, info2);
-    return 0;
-}
-
-
+    /* We might of VT switch from a non fbdev device */	
+    if (prev->conp && prev->fb_info) {	
+    	/* Save current video mode and colormap */
+    	memcpy(&prev->var,&prev->fb_info->var,sizeof(struct fb_var_screeninfo));
+    	fb_copy_cmap(&prev->fb_info->cmap, &prev->cmap, 0);
+    }	     
     /*
-     *  Blank the screen
+     * Install new video mode and colormap.
      */
-
-void fbgen_blank(int blank, struct fb_info *info)
-{
-    struct fb_info_gen *info2 = (struct fb_info_gen *)info;
-    struct fbgen_hwswitch *fbhw = info2->fbhw;
-    u16 black[16];
-    struct fb_cmap cmap;
-
-    if (fbhw->blank && !fbhw->blank(blank, info2))
-	return;
-    if (blank) {
-	memset(black, 0, 16*sizeof(u16));
-	cmap.red = black;
-	cmap.green = black;
-	cmap.blue = black;
-	cmap.transp = NULL;
-	cmap.start = 0;
-	cmap.len = 16;
-	fb_set_cmap(&cmap, 1, info);
-    } else
-	fbgen_install_cmap(currcon, info2);
+    new->fb_info->display_fg = vc_cons[con].d;
+    new->fb_info->display_fg->vc_num = con;
+    fb_copy_cmap(&new->cmap, &new->fb_info->cmap, 0);
+    new->fb_info->fbops->fb_set_var(&new->var, con, new->fb_info);
+    return 0;
 }
