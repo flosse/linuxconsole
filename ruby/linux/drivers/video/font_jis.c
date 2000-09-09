4c9549f6ac3d6d2e73b4dc4e1c187cdab11c0eeb
James Simmons <jsimmons@users.sf.net>
2000-09-09 19:23:24 +0000

    Added japanese font support.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@728 81ba4a36-902d-0410-9329-aea0610344cf

---
+++ ruby/linux/drivers/video/font_jis.c
@@ -0,0 +1,60 @@
+/*
+ * linux/video/encode_big5.c
+ *
+ * Copyright (C) 1999		Christopher Li, Jim Chen
+ *				GNU/Linux Research Center
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file COPYING in the main directory of this archive
+ * for more details.
+ *
+ *
+ */
+
+
+#include <linux/module.h>
+#include <linux/fb_doublebyte.h>
+#include "jis16.h"
+#define min1 0xa1
+#define max1 0xf9
+
+
+int is_left(int c)
+{
+	return ( c >= min1 && c<=max1);
+}
+int is_right(int c)
+{
+	return ( c >= 0x40 && c<=0x7e || c>=0xa1 && c <= 0xfe);
+}
+int index_jis (int ch1, int ch2)
+{
+	if (!is_left(ch1) || !is_right(ch2)) return -512;
+	if (ch1 > 0x2A)
+		return ((ch2 - 0x41 + (ch1 - 0x26) * 96) << 5);
+	else
+		return ((ch2 - 0x21 + (ch1 - 0x21) * 96) << 5);
+}
+struct double_byte db_gb =
+{
+	0,
+	"JIS5",
+	is_left,
+	is_right,
+	index_jis,
+	16,16,
+	max_jis16,
+	font_jis16
+};
+
+int init_module(void)
+{
+	if (doublebyte_default) return 1;
+	doublebyte_default = &db_gb;
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	doublebyte_default = (void*) 0;
+}	
