05b6ce239581e4ae72236181ebeaa79840c16c85
James Simmons <jsimmons@users.sf.net>
2000-09-10 13:20:57 +0000

    Synced with test8
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@732 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/drivers/video/matrox/matroxfb_crtc2.c
+++ ruby/linux/drivers/video/matrox/matroxfb_crtc2.c
@@ -464,7 +464,8 @@ static int matroxfb_dh_ioctl(struct inode* inode,
 				err = matroxfb_dh_get_vblank(m2info, &vblank);
 				if (err)
 					return err;
-				copy_to_user_ret((struct fb_vblank*)arg, &vblank, sizeof(vblank), -EFAULT);
+				if (copy_to_user((struct fb_vblank*)arg, &vblank, sizeof(vblank)))
+					return -EFAULT;
 				return 0;
 			}
 		case MATROXFB_SET_OUTPUT_MODE:
@@ -477,7 +478,8 @@ static int matroxfb_dh_ioctl(struct inode* inode,
 			{
 				u_int32_t tmp;
 
-				get_user_ret(tmp, (u_int32_t*)arg, -EFAULT);
+				if (get_user(tmp, (u_int32_t*)arg))
+					return -EFAULT;
 				if (tmp & ~ACCESS_FBINFO(output.all))
 					return -EINVAL;
 				if (tmp & ACCESS_FBINFO(output.ph))
@@ -494,7 +496,9 @@ static int matroxfb_dh_ioctl(struct inode* inode,
 			}
 		case MATROXFB_GET_OUTPUT_CONNECTION:
 			{
-				put_user_ret(ACCESS_FBINFO(output.sh), (u_int32_t*)arg, -EFAULT);
+				if (put_user(ACCESS_FBINFO(output.sh), 
+					     (u_int32_t*)arg))
+					return -EFAULT;
 				return 0;
 			}
 		case MATROXFB_GET_AVAILABLE_OUTPUTS:
