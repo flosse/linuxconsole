38bbd132ca84c699ce76950e706b64d42e9a0d26
Johann Deneux <deneux@ifrance.com>
2001-05-07 22:15:54 +0000

    New util testing force feedback devices.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@1191 81ba4a36-902d-0410-9329-aea0610344cf

---
+++ ruby/utils/fftest.c
@@ -0,0 +1,158 @@
+/*
+ * Tests the force feedback driver
+ * Copyright 2001 Johann Deneux <deneux@ifrance.com>
+ */
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * You can contact the author by email at this address:
+ * Johann Deneux <deneux@ifrance.com>
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/types.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+
+#define BIT(x) (1<<(x))
+
+#define N_EFFECTS 2
+
+int main(int argc, char** argv)
+{
+	struct ff_effect effects[N_EFFECTS];
+	struct input_event play, stop;
+	int fd;
+	char device_file_name[64];
+	unsigned long axes;
+	int i;
+
+	strncpy(device_file_name, "/dev/input/event0", 64);
+
+	for (i=1; i<argc; ++i) {
+		if (strncmp(argv[i], "--help", 64) == 0) {
+			printf("Usage: %s /dev/eventXX\n", argv[0]);
+			printf("Tests the force feedback driver\n");
+			exit(1);
+		}
+		else {
+			strncpy(device_file_name, argv[i], 64);
+		}
+	}
+
+	/* Open device */
+	fd = open(device_file_name, O_RDWR);
+	if (fd == -1) {
+		perror("Open device file");
+		exit(1);
+	}
+	printf("Device %s opened\n", device_file_name);
+
+	/* Query device */
+	if (ioctl(fd, EVIOCGBIT(EV_FF, sizeof(unsigned long)), &axes) == -1) {
+		perror("Ioctl query");
+		exit(1);
+	}
+	printf("Axes query: ");
+	if (axes == 0) {
+		printf("No axes, strange ?!");
+	}
+	else {
+		if (axes & BIT(FF_X)) printf("Axis X ");
+		if (axes & BIT(FF_Y)) printf("Axis Y ");
+	}
+	printf("\n");
+
+	/* download a constant effect */
+	effects[1].type = FF_CONSTANT;
+	effects[1].id = 0;
+	effects[1].u.constant.level = 0x4000;	/* Strength : 50 % */
+	effects[1].u.constant.direction = 0x6000;	/* 135 degrees */
+	effects[1].u.constant.shape.attack_length = 0x100;
+	effects[1].u.constant.shape.attack_level = 0;
+	effects[1].u.constant.shape.fade_length = 0x100;
+	effects[1].u.constant.shape.fade_level = 0;
+	effects[1].u.constant.trigger.button = 0;
+	effects[1].u.constant.trigger.interval = 0;
+	effects[1].u.constant.replay.length = 0x1400;  /* 20 seconds */
+	effects[1].u.constant.replay.delay = 0;
+
+	if (ioctl(fd, EVIOCSFF, &effects[1]) == -1) {
+		perror("Upload effects[1]");
+		exit(1);
+	}
+
+	/* download a periodic sinusoidal effect */
+	effects[0].type = FF_PERIODIC;
+	effects[0].id = 1;
+	effects[0].u.periodic.waveform = FF_SINE;
+	effects[0].u.periodic.period = 0x100;	/* 1 second */
+	effects[0].u.periodic.magnitude = 0x2000;	/* 0.25 * Maximum magnitude */
+	effects[0].u.periodic.offset = 0;
+	effects[0].u.periodic.phase = 0;
+	effects[0].u.periodic.direction = 0x4000;	/* Along X axis */
+	effects[0].u.periodic.shape.attack_length = 0x100;
+	effects[0].u.periodic.shape.attack_level = 0;
+	effects[0].u.periodic.shape.fade_length = 0x100;
+	effects[0].u.periodic.shape.fade_level = 0;
+	effects[0].u.periodic.trigger.button = 0;
+	effects[0].u.periodic.trigger.interval = 0;
+	effects[0].u.periodic.replay.length = 0x1400;  /* 20 seconds */
+	effects[0].u.periodic.replay.delay = 0;
+
+	if (ioctl(fd, EVIOCSFF, &effects[0]) == -1) {
+		perror("Upload effects[0]");
+		exit(1);
+	}
+
+	/* Ask user what effects to play */
+	do {
+		printf("Enter effect number, -1 to exit\n");
+		scanf("%d", &i);
+		if (i >= 0 && i < N_EFFECTS) {
+			play.type = EV_FF;
+			play.code = FF_PLAY | effects[i].id;
+			play.value = 1;
+
+			if (write(fd, (const void*) &play, sizeof(play)) == -1) {
+				perror("Play effect");
+				exit(1);
+			}
+		}
+		else {
+			printf("No such effect\n");
+		}
+	} while (i>=0);
+
+	/* Stop the effects */
+	for (i=0; i<N_EFFECTS; ++i) {
+		stop.type = EV_FF;
+		stop.code = FF_STOP | effects[i].id;
+        
+		if (write(fd, (const void*) &stop, sizeof(stop)) == -1) {
+			perror("Stop effect");
+			exit(1);
+		}
+	}
+	
+
+	exit(0);
+}
