965b47812bfdeef7e288f8ee29b15196064d8c0b
Vojtech Pavlik <vojtech@suse.cz>
2001-05-23 13:01:26 +0000

    Fix a missing "
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@1286 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/drivers/input/sidewinder.c
+++ ruby/linux/drivers/input/sidewinder.c
@@ -582,7 +582,7 @@ static void sw_connect(struct gameport *gameport, struct gameport_dev *dev)
 	if (gameport_open(gameport, dev, GAMEPORT_MODE_RAW))
 		goto fail1;
 
-	dbg("Init 0: Opened gameport %d, io %#x, speed %d,
+	dbg("Init 0: Opened gameport %d, io %#x, speed %d",
 		gameport->number, gameport->io, gameport->speed);
 
 	i = sw_read_packet(gameport, buf, SW_LENGTH, 0);		/* Read normal packet */
