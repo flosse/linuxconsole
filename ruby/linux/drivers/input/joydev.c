3b0b24c7d01bd0ebf01d60a43b2a45d3e2902fc0
Johann Deneux <deneux@ifrance.com>
2001-06-02 23:10:38 +0000

    Small fix in joydev_connect() (iforce wheels would not be connected).
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@1355 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/drivers/input/joydev.c
+++ ruby/linux/drivers/input/joydev.c
@@ -413,7 +413,8 @@ static struct input_handle *joydev_connect(struct input_handler *handler, struct
 	int i, j, minor;
 
 	if (!(test_bit(EV_KEY, dev->evbit) && test_bit(EV_ABS, dev->evbit) &&
-	     (test_bit(ABS_X, dev->absbit) || test_bit(ABS_Y, dev->absbit)) &&
+	     (test_bit(ABS_X, dev->absbit) || test_bit(ABS_Y, dev->absbit)
+		|| test_bit(ABS_WHEEL, dev->absbit)) &&
 	     (test_bit(BTN_TRIGGER, dev->keybit) || test_bit(BTN_A, dev->keybit)
 		|| test_bit(BTN_1, dev->keybit)))) return NULL; 
 
