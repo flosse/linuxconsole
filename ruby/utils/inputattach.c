89c128b96d5b4d2216f6c9ce5cabe058b5e28e26
Vojtech Pavlik <vojtech@suse.cz>
2001-06-17 20:10:46 +0000

    Add full Spaceball support.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@1429 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/utils/inputattach.c
+++ ruby/utils/inputattach.c
@@ -63,14 +63,7 @@ int readchar(int fd, unsigned char *c, int timeout)
 	return 0;
 }
 
-int waitchar(int fd, unsigned char c, int timeout)
-{
-	unsigned char b = 0;
 
-	while (readchar(fd, &b, timeout) || b != c);
-
-	return -(b != c);
-}
 
 void setline(int fd, int flags, int speed)
 {
@@ -105,47 +98,132 @@ int logitech_command(int fd, char *c)
 	return 0;
 }
 
-int magellan_init(int fd)
+int magellan_init(int fd, long *id, long *extra)
 {
 	write(fd, "m3\rpBB\rz\r", 9);
 	return 0;
 }
 
-int warrior_init(int fd)
+int warrior_init(int fd, long *id, long *extra)
 {
 	if (logitech_command(fd, "*S")) return -1;
 	setline(fd, CS8, B4800);
 	return 0;
 }
 
-int spaceball_cmd(int fd, char *c)
+int spaceball_waitchar(int fd, unsigned char c, unsigned char *d, int timeout)
+{
+	unsigned char b = 0;
+
+	while (!readchar(fd, &b, timeout)) {
+		if (b == 0x0a) continue;
+		*d++ = b;
+		if (b == c) break;
+	}
+
+	*d = 0;
+
+	return -(b != c);
+}
+
+int spaceball_waitcmd(int fd, char c, char *d)
 {
 	int i;
+
+	for (i = 0; i < 8; i++) {
+		if (spaceball_waitchar(fd, 0x0d, d, 1000))
+			return -1;
+		if (d[0] == c)
+			return 0;
+	}
+
+	return -1;
+}
+
+int spaceball_cmd(int fd, char *c, char *d)
+{
+	int i;
+
 	for (i = 0; c[i]; i++)
 		write(fd, c + i, 1);
 	write(fd, "\r", 1);
-	if (waitchar(fd, 0x0d, 4000))
+
+	i = spaceball_waitcmd(fd, toupper(c[0]), d);
+
+	return i;
+}
+
+#define SPACEBALL_1003		1
+#define SPACEBALL_2003B		3
+#define SPACEBALL_2003C		4
+#define SPACEBALL_3003C		7
+#define SPACEBALL_4000FLX	8
+#define SPACEBALL_4000FLX_L 	9
+
+int spaceball_init(int fd, long *id, long *extra)
+{
+	char r[64];
+
+	if (spaceball_waitchar(fd, 0x11, r, 4000) ||
+	    spaceball_waitchar(fd, 0x0d, r, 1000))
+		return -1;
+
+	if (spaceball_waitcmd(fd, '@', r))
+		return -1; 
+
+	if (strncmp("@1 Spaceball alive", r, 18))
+		return -1;
+
+	if (spaceball_waitcmd(fd, '@', r))
+		return -1; 
+
+	if (spaceball_cmd(fd, "hm", r))
+		return -1;
+
+	if (!strncmp("Hm2003B", r, 7))
+		*id = SPACEBALL_2003B;
+	if (!strncmp("Hm2003C", r, 7))
+		*id = SPACEBALL_2003C;
+	if (!strncmp("Hm3003C", r, 7))
+		*id = SPACEBALL_3003C;
+
+	if (!strncmp("HvFirmware", r, 10)) {
+
+		if (spaceball_cmd(fd, "\"", r))
+			return -1;
+
+		if (strncmp("\"1 Spaceball 4000 FLX", r, 21))
+			return -1;
+
+		if (spaceball_waitcmd(fd, '"', r))
+			return -1; 
+
+		if (strstr(r, " L "))
+			*id = SPACEBALL_4000FLX_L;
+		else
+			*id = SPACEBALL_4000FLX;
+
+		if (spaceball_waitcmd(fd, '"', r))
+			return -1; 
+
+		if (spaceball_cmd(fd, "YS", r))
+        	        return -1;
+
+		if (spaceball_cmd(fd, "M", r))
+        	        return -1;
+
+		return 0;
+	}
+
+	if (spaceball_cmd(fd, "P@A@A", r) ||
+	    spaceball_cmd(fd, "FT@", r)   ||
+	    spaceball_cmd(fd, "MSS", r))
 		return -1;
 
 	return 0;
 }
 
-int spaceball_init(int fd)
-{
-	if (	waitchar(fd, 0x11, 4000)   ||
-		waitchar(fd, 0x0d, 1000)   ||
-		waitchar(fd, 0x0d, 4000)   ||
-		waitchar(fd, 0x0d, 4000)   ||
-		spaceball_cmd(fd, "YS")    ||
-		spaceball_cmd(fd, "P@A@A") ||
-		spaceball_cmd(fd, "FT@")   ||
-		spaceball_cmd(fd, "MSS"))
-			return -1;
-
-	return 0;
-}
-
-int stinger_init(int fd)
+int stinger_init(int fd, long *id, long *extra)
 {
 	int i;
 	unsigned char c;
@@ -161,14 +239,14 @@ int stinger_init(int fd)
 	return 0;
 }
 
-int mzp_init(int fd)
+int mzp_init(int fd, long *id, long *extra)
 {
 	if (logitech_command(fd, "*X*q")) return -1;
 	setline(fd, CS8, B9600);
 	return 0;
 }
 
-int newton_init(int fd)
+int newton_init(int fd, long *id, long *extra)
 {
   int i;
   unsigned char c;
@@ -186,7 +264,7 @@ int newton_init(int fd)
   return 0;
 }
 
-int dump_init(int fd)
+int dump_init(int fd, long *id, long *extra)
 {
 	unsigned char c, o = 0;
 
@@ -210,14 +288,14 @@ struct input_types {
 	unsigned long type;
 	unsigned long extra;
 	int flush;
-	int (*init)(int fd);
+	int (*init)(int fd, long *id, long *extra);
 };
 
 struct input_types input_types[] = {
 
 { "--sunkbd",		"-skb",		B1200, CS8,			SERIO_SUNKBD,	0x00,	1,	NULL },
 { "--spaceorb",		"-orb",		B9600, CS8,			SERIO_SPACEORB,	0x00,	1,	NULL },
-{ "--spaceball",	"-sbl",		B9600, CS8,			SERIO_SPACEBALL,0x00,	1,	spaceball_init },
+{ "--spaceball",	"-sbl",		B9600, CS8,			SERIO_SPACEBALL,0x00,	0,	spaceball_init },
 { "--magellan",		"-mag",		B9600, CS8 | CSTOPB | CRTSCTS,	SERIO_MAGELLAN,	0x00,	1,	magellan_init },
 { "--warrior",		"-war",		B1200, CS7 | CSTOPB,		SERIO_WARRIOR,	0x00,	1,	warrior_init },
 { "--stinger",		"-sting",	B1200, CS8,			SERIO_STINGER,	0x00,	1,	stinger_init },
@@ -229,8 +307,8 @@ struct input_types input_types[] = {
 { "--intellimouse",	"-ms3",		B1200, CS7,			SERIO_MZ,	0x11,	1,	NULL },
 { "--mmwheel",		"-mmw",		B1200, CS7 | CSTOPB,		SERIO_MZP,	0x13,	1,	mzp_init },
 { "--iforce",		"-ifor",	B38400, CS8,			SERIO_IFORCE,	0x00,	0,	NULL },
-{ "--newtonkbd",        "-newt",        B9600, CS8,                     SERIO_NEWTON,  0x00,    0,      newton_init },
-{ "--h3600ts",          "-ipaq",     	B115200, CS8,                   SERIO_H3600,   0x00,    0,      NULL },
+{ "--newtonkbd",        "-newt",        B9600, CS8,                     SERIO_NEWTON,	0x00,   0,      newton_init },
+{ "--h3600ts",          "-ipaq",     	B115200, CS8,                   SERIO_H3600,	0x00,   0,      NULL },
 { "--stowawaykbd",      "-ipaqkbd",     B115200, CS8,                   SERIO_STOWAWAY, 0x00,   0,      NULL },
 { "--ps2serkbd",	"-ps2ser",	B1200, CS8,			SERIO_PS2SER,	0x00,	1,	NULL },
 { "--dump",		"-dump",	B1200, CS7, 			0,		0x00,	0,	dump_init },
@@ -243,6 +321,7 @@ int main(int argc, char **argv)
 	unsigned long devt;
 	int ldisc;
         int type;
+	long id, extra;
         int fd;
 	char c;
 
@@ -293,7 +372,10 @@ int main(int argc, char **argv)
 	if (input_types[type].flush)
 		while (!readchar(fd, &c, 100));
 
-	if (input_types[type].init && input_types[type].init(fd)) {
+	id = 0;
+	extra = input_types[type].extra;
+
+	if (input_types[type].init && input_types[type].init(fd, &id, &extra)) {
 		fprintf(stderr, "inputattach: device initialization failed\n");
 		return 1;
 	}
@@ -304,7 +386,7 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	devt = SERIO_RS232 | input_types[type].type | (input_types[type].extra << 16);
+	devt = SERIO_RS232 | input_types[type].type | (id << 8) | (extra << 16);
 
 	if(ioctl(fd, SPIOCSTYPE, &devt)) {
 		fprintf(stderr, "inputattach: can't set device type\n");
