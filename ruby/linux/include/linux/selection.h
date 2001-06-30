624ac0cc0d32fc39ef30790586597aa9e2555b40
James Simmons <jsimmons@users.sf.net>
2001-06-30 17:39:25 +0000

    Removed useless extern and *vc which was causing problems on ARM. Soon to go away.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@1466 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/include/linux/selection.h
+++ ruby/linux/include/linux/selection.h
@@ -9,25 +9,24 @@
 
 #include <linux/vt_buffer.h>
 
-struct vc_data *vc;
 extern int sel_cons;
 
-extern void clear_selection(void);
-extern int set_selection(const unsigned long arg, struct tty_struct *tty, int user);
-extern int paste_selection(struct tty_struct *tty);
-extern int sel_loadlut(const unsigned long arg);
-extern int mouse_reporting(struct tty_struct *tty);
-extern void mouse_report(struct tty_struct *tty, int butt, int mrx, int mry);
+void clear_selection(void);
+int set_selection(const unsigned long arg, struct tty_struct *tty, int user);
+int paste_selection(struct tty_struct *tty);
+int sel_loadlut(const unsigned long arg);
+int mouse_reporting(struct tty_struct *tty);
+void mouse_report(struct tty_struct *tty, int butt, int mrx, int mry);
 
-extern unsigned short *screen_pos(struct vc_data *vc, int w_offset, int viewed);
-extern u16 screen_glyph(struct vc_data *vc, int offset);
-extern void complement_pos(struct vc_data *vc, int offset);
-extern void invert_screen(struct vc_data *vc, int offset, int count, int shift);
+unsigned short *screen_pos(struct vc_data *vc, int w_offset, int viewed);
+u16 screen_glyph(struct vc_data *vc, int offset);
+void complement_pos(struct vc_data *vc, int offset);
+void invert_screen(struct vc_data *vc, int offset, int count, int shift);
 
-extern void getconsxy(struct vc_data *vc, char *p);
-extern void putconsxy(struct vc_data *vc, char *p);
+void getconsxy(struct vc_data *vc, char *p);
+void putconsxy(struct vc_data *vc, char *p);
 
-extern u16 vcs_scr_readw(struct vc_data *vc, const u16 *org);
-extern void vcs_scr_writew(struct vc_data *vc, u16 val, u16 *org);
+u16 vcs_scr_readw(struct vc_data *vc, const u16 *org);
+void vcs_scr_writew(struct vc_data *vc, u16 val, u16 *org);
 
 #endif
