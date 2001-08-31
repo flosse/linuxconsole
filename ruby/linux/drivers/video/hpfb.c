648da7ae617164cd4f95128405f0cf32916003ff
James Simmons <jsimmons@users.sf.net>
2001-08-31 18:12:45 +0000

    Added more drivers comverted to the new api. They are untested and their is some flaky code.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@1628 81ba4a36-902d-0410-9329-aea0610344cf

---
+++ ruby/linux/drivers/video/hpfb.c
@@ -0,0 +1,232 @@
+/*
+ *	HP300 Topcat framebuffer support (derived from macfb of all things)
+ *	Phil Blundell <philb@gnu.org> 1998
+ * 
+ * Should this be moved to drivers/dio/video/ ? -- Peter Maydell
+ * No! -- Jes
+ */
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/errno.h>
+#include <linux/string.h>
+#include <linux/mm.h>
+#include <linux/tty.h>
+#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/init.h>
+#include <linux/fb.h>
+#include <linux/dio.h>
+#include <asm/io.h>
+#include <asm/blinken.h>
+#include <asm/hwtest.h>
+
+static struct fb_info fb_info;
+
+unsigned long fb_regs;
+unsigned char fb_bitmask;
+
+#define TC_WEN		0x4088
+#define TC_REN		0x408c
+#define TC_FBEN		0x4090
+#define TC_NBLANK	0x4080
+
+/* blitter regs */
+#define BUSY		0x4044
+#define WMRR		0x40ef
+#define SOURCE_X	0x40f2
+#define SOURCE_Y	0x40f6
+#define DEST_X		0x40fa
+#define DEST_Y		0x40fe
+#define WHEIGHT		0x4106
+#define WWIDTH		0x4102
+#define WMOVE		0x409c
+
+static struct fb_fix_screeninfo hpfb_fix __initdata = {
+    "HP300 Topcat", (unsigned long) NULL, 1024*768, FB_TYPE_PACKED_PIXELS, 0,
+    FB_VISUAL_PSEUDOCOLOR, 0, 0, 0, 1024, (unsigned long) NULL, 0, FB_ACCEL_NONE};
+
+static struct fb_var_screeninfo hpfb_defined = {
+	1024,768,1024,768, 	/* W,H, W, H (virtual) load xres,xres_virtual*/
+	0,0,			/* virtual -> visible no offset */
+	1,			/* depth -> load bits_per_pixel */
+	0,			/* greyscale ? */
+	{0,2,0},		/* R */
+	{0,2,0},		/* G */
+	{0,2,0},		/* B */
+	{0,0,0},		/* transparency */
+	0,			/* standard pixel format */
+	FB_ACTIVATE_NOW,
+	274,195,		/* 14" monitor */
+	FB_ACCEL_NONE,
+	0L,0L,0L,0L,0L,
+	0L,0L,0,		/* No sync info */
+	FB_VMODE_NONINTERLACED,
+	{0,0,0,0,0,0}
+};
+
+static int hpfb_check_var(struct fb_var_screeninfo *var, struct fb_info *info)
+{
+	return -EINVAL;
+}
+
+/*
+ * Set the palette.  This may not work on all boards but only experimentation will tell.
+ * XXX Doesn't work at all.
+ */
+static int hpfb_setcolreg(unsigned regno, unsigned red, unsigned green,
+                           unsigned blue, unsigned transp,
+                           struct fb_info *info)
+{
+	while (readw(fb_regs + 0x6002) & 0x4) udelay(1);
+	writew(0, fb_regs + 0x60f0);
+	writew(regno, fb_regs + 0x60b8);
+	writew(red, fb_regs + 0x60b2);
+	writew(green, fb_regs + 0x60b4);
+	writew(blue, fb_regs + 0x60b6);
+	writew(0xff, fb_regs + 0x60f0);
+	udelay(100);
+	writew(0xffff, fb_regs + 0x60ba);
+	return 0;
+}
+
+void hpfb_copyarea(struct fb_info *info, int sx, int sy, unsigned int width,
+                   unsigned int height, int dx, int dy)
+{
+	while (readb(fb_regs + BUSY) & fb_bitmask);
+	writeb(0x3, fb_regs + WMRR);
+	writew(sx, fb_regs + SOURCE_X);
+	writew(sy, fb_regs + SOURCE_Y);
+	writew(dx, fb_regs + DEST_X);
+	writew(dy, fb_regs + DEST_Y);
+	writew(height, fb_regs + WHEIGHT);
+	writew(width, fb_regs + WWIDTH);
+	writeb(fb_bitmask, fb_regs + WMOVE);
+}
+
+static struct fb_ops hpfb_ops = {
+	owner:		THIS_MODULE,
+        fb_check_var:   hpfb_check_var,
+        fb_setcolreg:   hpfb_setcolreg,
+        fb_fillrect:    cfb_fillrect,
+        fb_copyarea:    hpfb_copyarea,
+        fb_imageblit:   cfb_imageblit,
+};
+
+#define TOPCAT_FBOMSB	0x5d
+#define TOPCAT_FBOLSB	0x5f
+
+int __init hpfb_init_one(unsigned long base)
+{
+	unsigned long fboff;
+
+	fboff = (readb(base + TOPCAT_FBOMSB) << 8) 
+		| readb(base + TOPCAT_FBOLSB);
+
+	hpfb_fix.smem_start = 0xf0000000 | (readb(base + fboff) << 16);
+	fb_regs = base;
+
+#if 0
+	/* This is the magic incantation NetBSD uses to make Catseye boards work. */
+	writeb(0, base+0x4800);
+	writeb(0, base+0x4510);
+	writeb(0, base+0x4512);
+	writeb(0, base+0x4514);
+	writeb(0, base+0x4516);
+	writeb(0x90, base+0x4206);
+#endif
+
+	/*
+	 *	Fill in the available video resolution
+	 */
+	 
+	hpfb_defined.xres = 1024;
+	hpfb_defined.yres = 768;
+	hpfb_defined.xres_virtual = 1024;
+	hpfb_defined.yres_virtual = 768;
+	hpfb_defined.bits_per_pixel = 8;
+
+	/* 
+	 *	Give the hardware a bit of a prod and work out how many bits per
+	 *	pixel are supported.
+	 */
+	
+	writeb(0xff, base + TC_WEN);
+	writeb(0xff, base + TC_FBEN);
+	writeb(0xff, hpfb_fix.smem_start);
+	fb_bitmask = readb(hpfb_fix.smem_start);
+
+	/*
+	 *	Enable reading/writing of all the planes.
+	 */
+	writeb(fb_bitmask, base + TC_WEN);
+	writeb(fb_bitmask, base + TC_REN);
+	writeb(fb_bitmask, base + TC_FBEN);
+	writeb(0x1, base + TC_NBLANK);
+
+	/*
+	 *	Let there be consoles..
+	 */
+	fb_info.node  = -1;
+	fb_info.fbops = &hpfb_ops;
+	fb_info.flags = FBINFO_FLAG_DEFAULT;
+	fb_info.var   = hpfb_defined;
+	fb_info.fix   = hpfb_fix;
+	fb_info.screen_base = hpfb_fix.smem_start;
+
+	if (register_framebuffer(&fb_info) < 0)
+		return 1;
+	return 0;
+}
+
+/* 
+ * Check that the secondary ID indicates that we have some hope of working with this
+ * framebuffer.  The catseye boards are pretty much like topcats and we can muddle through.
+ */
+
+#define topcat_sid_ok(x)  (((x) == DIO_ID2_LRCATSEYE) || ((x) == DIO_ID2_HRCCATSEYE)    \
+			   || ((x) == DIO_ID2_HRMCATSEYE) || ((x) == DIO_ID2_TOPCAT))
+
+/* 
+ * Initialise the framebuffer
+ */
+
+int __init hpfb_init(void)
+{
+	unsigned int sid;
+
+	/* Topcats can be on the internal IO bus or real DIO devices.
+	 * The internal variant sits at 0xf0560000; it has primary
+	 * and secondary ID registers just like the DIO version.
+	 * So we merge the two detection routines.
+	 *
+	 * Perhaps this #define should be in a global header file:
+	 * I believe it's common to all internal fbs, not just topcat.
+	 */
+#define INTFBADDR 0xf0560000
+
+	if (hwreg_present((void *)INTFBADDR) && (DIO_ID(INTFBADDR) == DIO_ID_FBUFFER)
+		&& topcat_sid_ok(sid = DIO_SECID(INTFBADDR)))
+	{
+		printk("Internal Topcat found (secondary id %02x)\n", sid); 
+		hpfb_init_one(INTFBADDR);
+	}
+	else
+	{
+		int sc = dio_find(DIO_ID_FBUFFER);
+		if (sc)
+		{
+			unsigned long addr = (unsigned long)dio_scodetoviraddr(sc);
+			unsigned int sid = DIO_SECID(addr);
+
+			if (topcat_sid_ok(sid))
+			{
+				printk("Topcat found at DIO select code %02x "
+				       "(secondary id %02x)\n", sc, sid);
+				hpfb_init_one(addr);
+			}
+		}
+	}
+	return 0;
+}
