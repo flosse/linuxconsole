c76bfbb1bcc31d93ae18a5958185c13deb43fdf3
Romain Dolbeau <Romain.Dolbeau@irisa.fr>
2001-10-03 08:46:38 +0000

    acceltest: add -h
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@1702 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/utils/acceltest.c
+++ ruby/utils/acceltest.c
@@ -46,7 +46,7 @@ main(int argc, char **argv)
 	struct fb_fillrect fbfr;
 	struct fb_copyarea fbca;
 	
-	while ((opnu = getopt(argc, argv, "f:x:y:w:h:c:")) != EOF)
+	while ((opnu = getopt(argc, argv, "f:x:y:w:h:c:"h)) != EOF)
 	{
 		switch(opnu)
 		{
@@ -73,8 +73,11 @@ main(int argc, char **argv)
 		case 'c':
 			color = atoi(optarg);
 			break;
+		case 'h':
+			printf("Usage: %s [-f <fb_device>] [-x <initial_X>] [-y <initial_Y>] [-w <width>] [-h <height>] [-c <colour>]", argv[0]);
+			break;
 		default:
-			fprintf(stderr, "Warning: Unknown option \"%c\"\n", opnu);
+			fprintf(stderr, "Warning: Unknown option \"%c\", try %s -h\n", opnu, argv[0]);
 		}
 	}
 
