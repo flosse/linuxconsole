9aac2c835b39210ed9f486addb238f3ebf747e27
Johann Deneux <deneux@ifrance.com>
2002-02-15 20:47:46 +0000

    Small fix.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@2030 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/utils/ffcfstress.c
+++ ruby/utils/ffcfstress.c
@@ -20,7 +20,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "../linux/include/linux/input.h"
+#include <linux/input.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
 #include <stdlib.h>
