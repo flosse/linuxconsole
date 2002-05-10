28898a28da39a366af1f6e4e3eb540bd2eded48a
James Simmons <jsimmons@users.sf.net>
2002-05-10 16:27:23 +0000

    Updates and fixes for the soft accel functions.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@2167 81ba4a36-902d-0410-9329-aea0610344cf

--- ruby/linux/drivers/video/cfbcopyarea.c
+++ ruby/linux/drivers/video/cfbcopyarea.c
@@ -26,19 +26,21 @@
 #include <linux/slab.h>
 #include <asm/types.h>
 #include <asm/io.h>
+#include <video/fbcon.h>
 
-void cfb_copyarea(struct fb_info *p, struct fb_copyarea *area) 
+void cfb_copyarea(struct fb_info *p, struct fb_copyarea *area)
 {
-	unsigned long start_index, end_index, start_mask, end_mask, last, tmp, height;
-	int x2, y2, n, j, lineincr, shift, shift_right, shift_left, old_dx, old_dy;
-	int linesize = p->fix.line_length, bpl = sizeof(unsigned long);
+	int x2, y2, lineincr, shift, shift_right, shift_left, old_dx, old_dy;
+	int n, j, linesize = p->fix.line_length, bpl = sizeof(unsigned long);
+	unsigned long start_index, end_index, start_mask, end_mask, last;
 	unsigned long *dst = NULL, *src = NULL;
-	char *src1,*dst1;
- 
+	char *src1, *dst1;
+	int tmp, height;
+
 	/* clip the destination */
 	old_dx = area->dx;
 	old_dy = area->dy;
-  
+
 	/*
 	 * We could use hardware clipping but on many cards you get around
 	 * hardware clipping by writing to framebuffer directly.
@@ -51,54 +53,56 @@ void cfb_copyarea(struct fb_info *p, struct fb_copyarea *area)
 	y2 = y2 < p->var.yres_virtual ? y2 : p->var.yres_virtual;
 	area->width = x2 - area->dx;
 	area->height = y2 - area->dy;
-  
+
 	/* update sx1,sy1 */
 	area->sx += (area->dx - old_dx);
 	area->sy += (area->dy - old_dy);
-  
+
 	height = area->height;
 
 	/* the source must be completely inside the virtual screen */
 	if (area->sx < 0 || area->sy < 0 ||
-	   (area->sx + area->width)  > p->var.xres_virtual ||
-	   (area->sy + area->height) > p->var.yres_virtual)
+	    (area->sx + area->width) > p->var.xres_virtual ||
+	    (area->sy + area->height) > p->var.yres_virtual)
 		return;
-  
-	if (area->dy < area->sy || (area->dy == area->sy && area->dx < area->sx)) {
+
+	if (area->dy < area->sy
+	    || (area->dy == area->sy && area->dx < area->sx)) {
 		/* start at the top */
-		src1 = p->screen_base + area->sy * linesize + 
-			((area->sx * p->var.bits_per_pixel) >> 3);
+		src1 = p->screen_base + area->sy * linesize +
+		    ((area->sx * p->var.bits_per_pixel) >> 3);
 		dst1 = p->screen_base + area->dy * linesize +
-			((area->dx * p->var.bits_per_pixel) >> 3);
+		    ((area->dx * p->var.bits_per_pixel) >> 3);
 		lineincr = linesize;
 	} else {
 		/* start at the bottom */
-		src1 = p->screen_base + (area->sy + area->height-1)*linesize + 
-			(((area->sx + area->width - 1) * p->var.bits_per_pixel) >> 3); 
-		dst1 = p->screen_base + (area->dy + area->height-1)*linesize + 
-			(((area->dx + area->width - 1) * p->var.bits_per_pixel) >> 3); 
+		src1 = p->screen_base + (area->sy + area->height-1) * linesize
+			+ (((area->sx + area->width - 1) * p->var.bits_per_pixel) >> 3);
+		dst1 = p->screen_base + (area->dy + area->height-1) * linesize
+			+ (((area->dx + area->width - 1) * p->var.bits_per_pixel) >> 3);
 		lineincr = -linesize;
 	}
-    
-	if ((BITS_PER_LONG % p->var.bits_per_pixel) == 0) {
-		int ppw = BITS_PER_LONG/p->var.bits_per_pixel;
-		int n = ((area->width * p->var.bits_per_pixel) >> 3);   
 
-		start_index = ((unsigned long) src1 & (bpl-1));
-		end_index = ((unsigned long) (src1 + n) & (bpl-1));
-		shift = ((unsigned long)dst1 & (bpl-1)) - ((unsigned long) src1 & (bpl-1));
+	if ((BITS_PER_LONG % p->var.bits_per_pixel) == 0) {
+		int ppw = BITS_PER_LONG / p->var.bits_per_pixel;
+		int n = ((area->width * p->var.bits_per_pixel) >> 3);
+
+		start_index = ((unsigned long) src1 & (bpl - 1));
+		end_index = ((unsigned long) (src1 + n) & (bpl - 1));
+		shift = ((unsigned long) dst1 & (bpl - 1)) -
+		    	((unsigned long) src1 & (bpl - 1));
 		start_mask = end_mask = 0;
-   
-		if (start_index) { 
+
+		if (start_index) {
 			start_mask = -1 >> (start_index << 3);
 			n -= (bpl - start_index);
 		}
-	
+
 		if (end_index) {
 			end_mask = -1 << ((bpl - end_index) << 3);
 			n -= end_index;
-		}   
-		n = n/bpl;
+		}
+		n = n / bpl;
 
 		if (n <= 0) {
 			if (start_mask) {
@@ -114,12 +118,16 @@ void cfb_copyarea(struct fb_info *p, struct fb_copyarea *area)
 		if (shift) {
 			if (shift > 0) {
 				/* dest is over to right more */
-				shift_right = shift * p->var.bits_per_pixel;
-				shift_left = (ppw - shift) * p->var.bits_per_pixel;
+				shift_right =
+				    shift * p->var.bits_per_pixel;
+				shift_left =
+				    (ppw - shift) * p->var.bits_per_pixel;
 			} else {
 				/* source is to the right more */
-				shift_right = (ppw + shift) * p->var.bits_per_pixel;
-				shift_left = -shift * p->var.bits_per_pixel;
+				shift_right =
+				    (ppw + shift) * p->var.bits_per_pixel;
+				shift_left =
+				    -shift * p->var.bits_per_pixel;
 			}
 			/* general case, positive increment */
 			if (lineincr > 0) {
@@ -137,8 +145,7 @@ void cfb_copyarea(struct fb_info *p, struct fb_copyarea *area)
 						dst++;
 						tmp = fb_readl(src);
 						src++;
-						fb_writel((last << shift_left) | 
-							 (tmp >> shift_right),dst);
+						fb_writel((last << shift_left) | (tmp >> shift_right), dst);
 						last = tmp;
 						src++;
 					}
@@ -162,8 +169,7 @@ void cfb_copyarea(struct fb_info *p, struct fb_copyarea *area)
 						dst--;
 						tmp = fb_readl(src);
 						src--;
-						fb_writel((tmp << shift_left) | 
-							 (last >> shift_right),dst);
+						fb_writel((tmp << shift_left) | (last >> shift_right), dst);
 						last = tmp;
 						src--;
 					}
@@ -175,24 +181,24 @@ void cfb_copyarea(struct fb_info *p, struct fb_copyarea *area)
 		} else {
 			/* no shift needed */
 			if (lineincr > 0) {
-				/* positive increment */	
+				/* positive increment */
 				do {
 					dst = (unsigned long *) (dst1 - start_index);
 					src = (unsigned long *) (src1 - start_index);
-   		
-					if (start_mask)	
+
+					if (start_mask)
 						fb_writel(fb_readl(src) | start_mask, dst);
-    			
-					for (j = 0; j < n; j++) { 
-						fb_writel(fb_readl(src), dst); 
+
+					for (j = 0; j < n; j++) {
+						fb_writel(fb_readl(src), dst);
 						dst++;
 						src++;
 					}
-    				
+
 					if (end_mask)
-						fb_writel(fb_readl(src) | end_mask,dst);
-						src1 += lineincr;
-						dst1 += lineincr;
+						fb_writel(fb_readl(src) | end_mask, dst);
+					src1 += lineincr;
+					dst1 += lineincr;
 				} while (--height);
 			} else {
 				/* negative increment */
@@ -202,8 +208,8 @@ void cfb_copyarea(struct fb_info *p, struct fb_copyarea *area)
 
 					if (start_mask)
 						fb_writel(fb_readl(src) | start_mask, dst);
-					for (j = 0; j < n; j++) { 
-						fb_writel(fb_readl(src), dst); 
+					for (j = 0; j < n; j++) {
+						fb_writel(fb_readl(src), dst);
 						dst--;
 						src--;
 					}
