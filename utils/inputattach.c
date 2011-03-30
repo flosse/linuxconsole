a8f224cc893d636d2e4367e8e9d4ecfec667e3cf
Stephen Kitt <steve@sk2.org>
2011-03-30 22:15:29 +0000

    Add support for the TAOS evaluation modules (driver i2c-taos-evm).
    Signed-off-by: Jean Delvare <jdelvare@suse.de>
    
    
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@2411 81ba4a36-902d-0410-9329-aea0610344cf

--- utils/inputattach.c
+++ utils/inputattach.c
@@ -580,6 +580,11 @@ static struct input_types input_types[] = {
 { "--zhen-hua",		"-zhen",	"Zhen Hua 5-byte protocol",
 	B19200, CS8,
 	SERIO_ZHENHUA,		0x00,	0x00,	0,	zhenhua_init },
+#ifdef SERIO_TAOSEVM
+{ "--taos-evm",		"-taos",	"TAOS evaluation module",
+	B1200, CS8,
+	SERIO_TAOSEVM,		0,	0,	0,	NULL },
+#endif
 { "--dump",		"-dump",	"Just enable device",
 	B2400, CS8,
 	0,			0x00,	0x00,	0,	dump_init },
