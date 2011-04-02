3e5bdbdcf19d4cd624e5c5ba51c529ca6fc95240
Stephen Kitt <steve@sk2.org>
2011-04-02 12:45:20 +0000

    Update FSF address and remove obsolete contact info.
    git-svn-id: https://linuxconsole.svn.sourceforge.net/svnroot/linuxconsole/trunk@2412 81ba4a36-902d-0410-9329-aea0610344cf

--- utils/jscal.c
+++ utils/jscal.c
@@ -23,11 +23,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *
- * Should you need to contact me, the author, you can do so either by
- * e-mail - mail your message to <vojtech@ucw.cz>, or by paper mail:
- * Vojtech Pavlik, Simunkova 1594, Prague 8, 182 00 Czech Republic
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
  */
 
 #include <sys/ioctl.h>
